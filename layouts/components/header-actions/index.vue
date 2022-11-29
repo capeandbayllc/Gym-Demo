7d56ecc021792321caf4880754382b7ce81f0c77
sandeepbacancy <sandeep.s@bacancy.com>
2022-11-29 11:06:11 +0530

    Create a new Notification Center Page

--- layouts/components/header-actions/index.vue
+++ layouts/components/header-actions/index.vue
@@ -14,7 +14,7 @@
 			<person-add-icon @click="showUserMenu"/>
 			<user-add-menu ref="userMenu"/>
 		</div>
-		<setting-icon />
+		<setting-icon class="setting-icon" />
 		<question-round-icon
 			@click="$emit('show-help')"
 		/>
@@ -23,6 +23,21 @@
 		/>
 	</div>
 </template>
+<style>
+	.alert-container:hover svg{
+		@apply rotate-12;
+	}
+	.alert-container:hover .notification{
+		@apply -right-2 left-auto z-10 ease-linear; 
+	}
+	.person-add-icon:hover path{
+		@apply opacity-100;
+	}
+	.setting-icon:hover{
+		animation: spin 1s linear 0.5; opacity: 0.5;
+	}
+	
+</style>
 <style scoped>
 .header-actions {
 	@apply flex items-center justify-end space-x-9 mr-8;
---
+++ pages/notification-center/components/notification-leads-member-card.vue
@@ -0,0 +1,112 @@
+<template>
+    <simple-card class="notification-leads-member-card -md:text-xs" :closable="false" title="Leads/Members">
+        <div class="notification-container">
+			<div class="notification-title">
+				<div class="notification-actions">
+					<select-box
+						class="w-36"
+						label="Leads"
+						:items="leadsList"
+						transparent
+						:secondary="false"
+					/>
+					<select-box
+						class="w-36"
+						label="Members"
+						:items="membersList"
+						:secondary="false"
+						transparent
+					/>
+					<search-input size="lg" border="neutral" neutral/>
+				</div>
+			</div>
+			<data-table
+                class="notification-leads-member-table"
+				:data="mockLeads"
+				:row-component="NotificationTableRow"
+				:columns="columns"
+			/>
+		</div>
+    </simple-card>
+</template>
+<style>
+.notification-leads-member-table .notification-table-row {
+	td, td > div {
+		@apply border-secondary;
+	}
+}
+</style>
+<style scoped>
+.notification-leads-member-card {
+    @apply m-auto mt-5 bg-neutral;
+	.notification-container {
+		@apply p-4;
+	}
+	.notification-title {
+		@apply flex flex-row justify-end items-center;
+		h3 {
+			@apply text-xl font-semibold pl-4;
+		}
+	}
+    .notification-actions {
+    	@apply flex flex-row space-x-8 justify-end mb-2 mt-5;
+    }
+}
+</style>
+<script setup>
+import NotificationTableRow from './notification-table-row.vue';
+const leadsList = [{
+	id: 1,
+	label: "Leads 1",
+	value: 1
+}, {
+	id: 2,
+	value: 2,
+	label: "Leads 2",
+}, {
+	id: 3,
+	value: 3,
+	label: "Leads 3",
+}]
+const membersList = [{
+	id: 1,
+	label: "Member 1",
+	value: 1
+}, {
+	id: 2,
+	value: 2,
+	label: "Member 2",
+}, {
+	id: 3,
+	value: 3,
+	label: "Member 3",
+}];
+const columns = ["Type", "Date/ Time", "Tags", "Members", "", "", ""];
+
+const mockLeads = [
+    {
+        id: 1,
+        type: "Upgrade Promo Response",
+        date: "2022-09-23",
+        time: "10:20 pm",
+        tag: "warning",
+        name: "Jack Smith",
+    }, 
+    {
+        id: 2,
+        type: "Upgrade Promo Response",
+        date: "2022-09-25",
+        time: "7:20 pm",
+        tag: "warning",
+        name: "George W.",
+    }, 
+    {
+        id: 3,
+        type: "Upgrade Promo Response",
+        date: "2022-09-23",
+        time: "5:30 pm",
+        tag: "warning",
+        name: "Jason Richards",
+    }
+];
+</script>
