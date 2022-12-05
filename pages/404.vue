22d9c07b3bbe15c5aeed77e4c38463271b4528e5
sandeepbacancy <sandeep.s@bacancy.com>
2022-12-05 16:35:12 +0530

    Add a wrapper div for UI update

--- pages/404.vue
+++ pages/404.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="mt-14 mx-auto">
-        <div class="text-center z-20">
-        <h1 class="text-9xl sm:text-10xl font-medium mb-8">404</h1>
-        <p class="text-xl px-8 sm:px-0 sm:text-4xl font-light mb-16 leading-tight">Page not found: {{$route.fullPath}}</p>
-        <div class="w-full flex items-center justify-center">
-            <NuxtLink to="/" class="relative gradient-border text-md sm:text-xl py-2 px-4 sm:py-3 sm:px-6 cursor-pointer">Go back home</NuxtLink>
-        </div>
+    <div>
+        <div class="mt-14 mx-auto">
+            <div class="text-center z-20">
+                <h1 class="text-9xl sm:text-10xl font-medium mb-8">404</h1>
+                <p class="text-xl px-8 sm:px-0 sm:text-4xl font-light mb-16 leading-tight">Page not found: {{$route.fullPath}}</p>
+                <div class="w-full flex items-center justify-center">
+                    <NuxtLink to="/" class="relative gradient-border text-md sm:text-xl py-2 px-4 sm:py-3 sm:px-6 cursor-pointer">Go back home</NuxtLink>
+                </div>
+            </div>
         </div>
     </div>
 </template>
