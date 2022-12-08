9e1a8b0a30a1b20b804ad0c5604349af13c44c54
ParthJasani123 <parth.jasani@bacancy.com>
2022-12-08 19:17:24 +0530

    Fix responsive in Club reference page

--- pages/club/index.vue
+++ pages/club/index.vue
@@ -3,9 +3,9 @@
         <div class="page-title">Club Reference</div>
         <div class="page-content">
             <simple-card title="Location Search">
-                <div class="px-12 py-6 bg-neutral">
+                <div class="px-4 md:px-12 py-6 bg-neutral">
                     <div class="location-filter">
-                        <div class="flex flex-row gap-5">
+                        <div class="location-filter-options">
                             <select-box :items="states" label="State" class="w-36"/>
                             <select-box :items="cities" label="City" class="w-36"/>
                         </div>
@@ -24,14 +24,28 @@
 </template>
 <style scoped>
 .page-club-container {
-    @apply py-4 pr-5 w-full h-fit pl-16;
+    @apply py-4 pr-5 bg-base-300 w-full h-fit border-b border-secondary pl-16 -md:pl-0;
+
     .page-title {
         @apply text-lg font-light pb-3 pl-5;
     }
     .page-content {
-        @apply flex border border-secondary bg-neutral rounded p-7 w-fit m-auto;
+        @apply flex flex-col-reverse md:flex-row gap-5 px-5 justify-center;
         .location-filter {
-            @apply flex flex-row justify-between mb-3;
+            @screen -md {
+                .search-input-wrapper {
+                    @apply	w-full;
+                }
+            }
+            @apply flex flex-col gap-2 md:flex-row justify-between mb-3;
+        }
+        .location-filter-options {
+            @screen -md {
+                .select-box-wrapper {
+                    @apply w-full;
+                }
+            }
+            @apply flex flex-col md:flex-row gap-2 md:gap-5
         }
         .club-table {
             @apply bg-neutral overflow-y-auto;
