dc21bc7e99d15e5dfa385160ba771c41d285c3f1
ParthJasani123 <parth.jasani@bacancy.com>
2023-01-18 16:30:13 +0530

    #66 Removed Titles from all the pages

--- pages/check-in/index.vue
+++ pages/check-in/index.vue
@@ -1,67 +1,86 @@
 <template>
-  <div class="w-full h-full flex flex-col">
-    <div class="page-checkin-container">
-      <div class="page-title">Member account page</div>
-      <div class="page-content">
-        <event-card />
-        <profile-card
-          @create-alert="showAlertAddModal"
-          @create-note="showNoteAddModal"
-          @select-option="option = $event"
-          :active-option="option"
+    <div class="w-full h-full flex flex-col">
+        <div class="page-checkin-container">
+            <div class="page-content">
+                <event-card />
+                <profile-card
+                    @create-alert="showAlertAddModal"
+                    @create-note="showNoteAddModal"
+                    @select-option="option = $event"
+                    :active-option="option"
+                />
+            </div>
+        </div>
+        <pos-card
+            v-if="option === 'pos'"
+            @close="option = null"
         />
-      </div>
+        <calendar-card
+            v-if="option === 'calendar'"
+            @close="option = null"
+        />
+        <notification-card
+            v-if="option === 'notification'"
+            @close="option = null"
+        />
+        <guest-card
+            v-if="option === 'guest-pass'"
+            @close="option = null"
+        />
+        <note-card
+            v-if="option === 'note'"
+            @close="option = null"
+        />
+        <footer-logo
+            class="m-auto"
+            v-else
+        />
+        <daisy-modal
+            id="alertAddModal"
+            ref="alertAddModal"
+            v-slot="scope"
+        >
+            <alert-add-modal @close="scope.close()" />
+        </daisy-modal>
+        <daisy-modal
+            id="noteAddModal"
+            ref="noteAddModal"
+            v-slot="scope"
+        >
+            <note-add-modal @close="scope.close()" />
+        </daisy-modal>
     </div>
-        <pos-card v-if="option === 'pos'" @close="option = null"/>
-        <calendar-card v-if="option === 'calendar'" @close="option = null"/>
-        <notification-card v-if="option === 'notification'" @close="option = null"/>
-        <guest-card v-if="option === 'guest-pass'" @close="option = null"/>
-        <note-card v-if="option === 'note'" @close="option = null"/>
-        <footer-logo class="m-auto" v-else/>
-    <daisy-modal id="alertAddModal" ref="alertAddModal" v-slot="scope">
-            <alert-add-modal @close="scope.close()"/>
-    </daisy-modal>
-    <daisy-modal id="noteAddModal" ref="noteAddModal" v-slot="scope">
-      <note-add-modal @close="scope.close()" />
-    </daisy-modal>
-        
-  </div>
 </template>
 <style scoped>
-
 .page-checkin-container {
-  @apply py-4 pr-5 bg-base-300 w-full h-fit border-b border-secondary pl-16 -md:pl-0;
-  .page-title {
-    @apply text-lg font-light pb-3 pl-5;
-  }
-  .page-content {
-    @apply flex flex-col-reverse md:flex-row gap-5  px-5 justify-center;
-  }
+    @apply py-4 pr-5 bg-base-300 w-full h-fit border-b border-secondary pl-16 -md:pl-0;
+    .page-content {
+        @apply flex flex-col-reverse md:flex-row gap-5  px-5 justify-center;
+    }
 }
 </style>
 <script setup>
-import {ref} from 'vue'
-import EventCard from './event-card/index.vue'
-import ProfileCard from './profile-card/index.vue'
-import PosCard from './pos-card/index.vue'
-import CalendarCard from './calendar-card/index.vue'
-import NotificationCard from './notification-card/index.vue'
-import GuestCard from './guest-card/index.vue'
-import NoteCard from './note-card/index.vue'
+import {ref} from 'vue';
+import EventCard from './event-card/index.vue';
+import ProfileCard from './profile-card/index.vue';
+import PosCard from './pos-card/index.vue';
+import CalendarCard from './calendar-card/index.vue';
+import NotificationCard from './notification-card/index.vue';
+import GuestCard from './guest-card/index.vue';
+import NoteCard from './note-card/index.vue';
 
-import AlertAddModal from './alert-add-modal.vue'
-import NoteAddModal from './note-add-modal.vue'
+import AlertAddModal from './alert-add-modal.vue';
+import NoteAddModal from './note-add-modal.vue';
 
+const option = ref(null);
 
-const option = ref(null)
-
-const alertAddModal = ref(null)
+const alertAddModal = ref(null);
 const showAlertAddModal = () => {
-    alertAddModal.value.open()
-}
+    alertAddModal.value.open();
+};
 
-const noteAddModal = ref(null)
+const noteAddModal = ref(null);
 const showNoteAddModal = () => {
-    noteAddModal.value.open()
-}
+    noteAddModal.value.open();
+};
 </script>
--- pages/engage/index.vue
+++ pages/engage/index.vue
@@ -1,11 +1,16 @@
 <template>
     <div class="page-engage-container">
-        <div class="page-title">Engage</div>
         <div class="page-content">
-            <tab-list v-model="activeTab"/>
+            <tab-list v-model="activeTab" />
             <div class="tab-view">
-                <component :is="tabViews[activeTab]" v-if="activeTab > -1"/>
-                <div class="w-64 h-56" v-else />
+                <component
+                    :is="tabViews[activeTab]"
+                    v-if="activeTab > -1"
+                />
+                <div
+                    class="w-64 h-56"
+                    v-else
+                />
             </div>
         </div>
     </div>
@@ -13,26 +18,31 @@
 <style scoped>
 .page-engage-container {
     @apply py-4 pr-5 w-full h-fit pl-16 -md:px-6;
-    .page-title {
-        @apply text-lg font-light pb-3 pl-5;
-    }
     .page-content {
         @apply flex flex-col items-center;
         /* margin-top: -2.25rem; */
-        .tab-view { 
+        .tab-view {
             @apply grid grid-cols-3 grid-rows-3 gap-4 border-2 border-secondary rounded py-5 px-4 bg-neutral;
         }
     }
 }
 </style>
 <script setup>
-import {ref} from 'vue'
-import TabList from './components/tab-list.vue'
-import EmployeeTab from './components/employee-tab/index.vue'
-import LeadTab from './components/lead-tab.vue'
-import MemberTab from './components/member-tab.vue'
-import FitnessTab from './components/fitness-tab.vue'
+import {ref} from 'vue';
+import TabList from './components/tab-list.vue';
+import EmployeeTab from './components/employee-tab/index.vue';
+import LeadTab from './components/lead-tab.vue';
+import MemberTab from './components/member-tab.vue';
+import FitnessTab from './components/fitness-tab.vue';
 
-const activeTab = ref(0)
-const tabViews = [EmployeeTab, LeadTab, MemberTab, FitnessTab, FitnessTab, FitnessTab, FitnessTab]
+const activeTab = ref(0);
+const tabViews = [
+    EmployeeTab,
+    LeadTab,
+    MemberTab,
+    FitnessTab,
+    FitnessTab,
+    FitnessTab,
+    FitnessTab,
+];
 </script>
