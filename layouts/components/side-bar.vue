a5021a6ca6ab3c53e0b8e522bd8d1ada17d17bd4
ParthJasani123 <parth.jasani@bacancy.com>
2023-02-03 19:47:15 +0530

    fixed height issue

--- layouts/components/side-bar.vue
+++ layouts/components/side-bar.vue
@@ -19,10 +19,10 @@
 </template>
 <style scoped>
 .side-bar {
-	@apply fixed right-0 z-20;
+	@apply absolute right-0 z-20;
 	/* height: calc(100vh - 6rem); */
 	&.collapsed {
-		@apply w-5 hover:w-7 bg-secondary;
+		@apply w-6 hover:w-7 bg-secondary;
 		@screen 3xl {
 			@apply hover:w-80;
 		}
--- layouts/default.vue
+++ layouts/default.vue
@@ -43,7 +43,7 @@
 	@apply flex flex-row justify-between relative;
 	.app-body-content {
 	    @apply flex flex-col w-full items-center;
-		height: calc(100vh - 6rem);
+		min-height: calc(100vh - 6rem);
 	}
 }
 </style>
