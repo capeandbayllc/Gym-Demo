8e9d4ca90a76c521d769032112cb7517e26d1918
ParthJasani123 <parth.jasani@bacancy.com>
2023-02-08 20:30:21 +0530

    FIxed the menu open issue in vendors, corporation, and recruitment pages in Engage

--- pages/corporation/components/corporation-table-row.vue
+++ pages/corporation/components/corporation-table-row.vue
@@ -9,10 +9,14 @@
                 <div class="btn btn-ghost btn-sm text-base-content" tabindex="0" @click.prevent.stop>
                     <font-awesome-icon icon="ellipsis-h" size="lg" />
                 </div>
-                <div class="dropdown-content menu p-2 shadow bg-black rounded w-52">
+                <div class="dropdown-content menu p-2 shadow bg-black rounded w-52 items-start">
+                    <div class="hover:bg-white uppercase hover:text-black border transition rounded border-secondary inline-block my-1 border-primary px-2 py-1 cursor-pointer text-secondary" tabindex="-1"  @click.stop="openEmailModal">+ Email</div>
+                    <div class="hover:bg-white uppercase hover:text-black border transition rounded border-secondary inline-block my-1 border-primary px-2 py-1 cursor-pointer text-secondary" tabindex="-1" @click.stop="outgoingCall">+ Call</div>
+                </div>
+                <!-- <div class="dropdown-content menu p-2 shadow bg-black rounded w-52">
                     <Button outline size="sm" class="my-1 border-primary text-secondary" @click="openEmailModal">+ Email</Button>
                     <Button outline size="sm" class="my-1 border-primary text-secondary" @click="outgoingCall">+ Call</Button>
-                </div>
+                </div> -->
                 <daisy-modal ref="outgoingCallModalRef">
                     <outgoing-call-modal @callNow="showInCallModal" @close="closeOutgoingCall"></outgoing-call-modal>
                 </daisy-modal>
