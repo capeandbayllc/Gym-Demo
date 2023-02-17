f7842d7786985a7edae575dabf7c4ca00dbba264
tommyleedev <tommy@capeandbay.com>
2023-02-17 11:47:50 -0500

    add: implemented calendar card for checkin page

--- pages/dashboard/components/calendar-card/index.vue
+++ pages/dashboard/components/calendar-card/index.vue
@@ -1,7 +1,16 @@
 <template>
     <dashboard-card :title-icon="CalendarIcon" title="March" class="flex flex-col justify-between">
-        <!-- <Datepicker v-model="date" inline auto-apply dark/> -->
-        <mock-calendar class="m-6"/>
+        <Datepicker
+            v-model="date"
+            inline
+            menu-class-name="dp-custom-menu"
+            calendar-class-name="dp-custom-calendar"
+            :day-class="getDayClass"
+            auto-apply
+            disable-month-year-select
+            :enable-time-picker="false"
+            :day-names="dayNames" 
+        />
         <div class="calendar-footer">
             <div class="calendar-footer-labels">
                 <div class="-md:text-xs">
@@ -29,6 +38,40 @@
     }
 }
 </style>
+<style>
+.dp-custom-menu {
+    @apply !bg-base-300 grid m-6 border-none;
+}
+.dp-custom-calendar {
+    .dp__calendar_header {
+        @apply text-base-content/80 font-light w-full mt-2;
+    }
+    .dp__calendar_header_separator {
+        @apply bg-transparent;
+    }
+    .dp__calendar {
+        @apply w-[30rem];
+    }
+    .dp__calendar_item {
+        @apply text-base-content flex flex-col items-center w-full;
+        &::after {
+            content: "";
+            @apply w-1.5 h-1.5 rounded-full;
+            @apply mt-1;
+        }
+        &:has(.event-yoga)::after {
+            @apply bg-secondary;
+        }
+        &:has(.event-pt)::after {
+            @apply bg-accent-focus;
+        }
+        &:has(.event-cycle)::after {
+            @apply bg-warning;
+        }
+    }
+
+}
+</style>
 <script setup>
 import { CalendarIcon } from '~~/components/icons'
 import DashboardCard from '../dashboard-card.vue'
@@ -37,4 +80,28 @@ import Datepicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
 
 const date = ref(new Date());
+
+const yoga = ["2023-02-01", "2023-02-02", "2023-02-05"]
+const pt = ["2023-02-17", "2023-02-18"]
+const cycle = ["2023-03-01", "2023-02-10"]
+const dayNames = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+const getDayClass = (date) => {
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    month = (month + "").padStart(2, "0");
+    let day = date.getDate();
+    day = (day + "").padStart(2, "0");
+    let str = `${year}-${month}-${day}`;
+    if (yoga.indexOf(str) > -1) {
+        return 'event-yoga'
+    }
+    if (pt.indexOf(str) > -1) {
+        return 'event-pt'
+    }
+    if (cycle.indexOf(str) > -1) {
+        return 'event-cycle'
+    }
+    return ''
+};
 </script>
