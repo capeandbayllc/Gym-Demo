811ea258361e07f17625c5a28c73be086174a021
Chrys Ugwu <chrys@capeandbay.com>
2023-03-01 19:16:04 -0600

    remove debug

--- pages/Kiosk.vue
+++ pages/Kiosk.vue
@@ -1,17 +1,9 @@
 <template>
-  111
-  <UserAddMenu ref="userMenu" mode="page"/>
+  <UserAddMenu mode="page"/>
 </template>
 
 <script setup>
 import UserAddMenu from "~/layouts/components/header-actions/user-add-menu.vue";
-import {ref} from "vue";
-
-const userMenu = ref(null);
-onMounted(() => {
-  // console.log(userMenu.value);
-  // userMenu.value.open();
-})
 </script>
 
 <style scoped>
