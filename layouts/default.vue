49d2e8320da03d6cf0da110b9b67bf0e62f0503a
Clayton Krogel <121507318+CnB-Clayton@users.noreply.github.com>
2023-03-01 19:29:34 -0500

    Revert "Center content"

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,61 +1,54 @@
 <template>
-    <div class="app-layout gradient-bg" v-if="isLoggedIn">
-        <app-header
-            @show-circular-menu="toggleCircularMenu"
-            @show-help="showBot"
-            @show-profile-menu="showProfileMenu"
-            class="sticky top-0 z-[22]"
-        />
-        <circular-menu v-if="showCircularMenu" @close="toggleCircularMenu" />
-        <help-bot ref="helpBot" />
-        <profile-menu
-            ref="profileMenu"
-            @isProfileMenuOpen="isProfileMenuOpenFn"
-        />
-        <div class="app-content">
-            <div class="w-auto">
-                <nav-bar />
-            </div>
-            <div class="relative">
-                <!-- <global-search
+  <div class="app-layout gradient-bg" v-if="isLoggedIn">
+    <app-header
+      @show-circular-menu="toggleCircularMenu"
+      @show-help="showBot"
+      @show-profile-menu="showProfileMenu"
+      class="sticky top-0 z-[22]"
+    />
+    <circular-menu v-if="showCircularMenu" @close="toggleCircularMenu" />
+    <help-bot ref="helpBot" />
+    <profile-menu ref="profileMenu" @isProfileMenuOpen="isProfileMenuOpenFn" />
+    <div class="app-content">
+      <nav-bar />
+      <div class="w-full relative">
+        <!-- <global-search
 					v-model="globalSearch"
 				/> -->
-                <div class="app-body">
-                    <div class="app-body-content">
-                        <slot />
-                    </div>
-                </div>
-            </div>
-            <div>
-                <side-bar />
-            </div>
+        <div class="app-body">
+          <div class="app-body-content">
+            <slot />
+          </div>
+          <side-bar />
         </div>
-        <ChatConversation />
-        <!-- <daisy-modal id="globalSearchModal" ref="globalSearchModal" :show-close-button="false">
+      </div>
+    </div>
+    <ChatConversation />
+    <!-- <daisy-modal id="globalSearchModal" ref="globalSearchModal" :show-close-button="false">
 			<global-search-modal />
 		</daisy-modal> -->
-    </div>
-    <div
-        class="gradient-bg min-h-screen w-screen flex flex-col items-center justify-center"
-        v-else
-    >
-        <slot />
-    </div>
+  </div>
+  <div
+    class="gradient-bg min-h-screen w-screen flex flex-col items-center justify-center"
+    v-else
+  >
+    <slot />
+  </div>
 </template>
 <style scoped>
 .app-layout {
-    @apply w-screen /* overflow-x-hidden */;
+  @apply w-screen /* overflow-x-hidden */;
 }
 .app-content {
-    @apply flex flex-row relative justify-between;
-    min-height: calc(100vh - 4rem);
+  @apply flex flex-row relative;
+  min-height: calc(100vh - 4rem);
 }
 .app-body {
-    @apply flex flex-row justify-between relative h-full;
-    .app-body-content {
-        @apply flex flex-col w-full items-center h-full py-8;
-        /* height: calc(100vh - 6rem); */
-    }
+  @apply flex flex-row justify-between relative h-full;
+  .app-body-content {
+    @apply flex flex-col w-full items-center h-full py-8;
+    /* height: calc(100vh - 6rem); */
+  }
 }
 </style>
 <script setup>
@@ -75,7 +68,7 @@ import ChatConversation from "./components/chat-conversation/index.vue";
 const showCircularMenu = ref(false);
 
 const toggleCircularMenu = () => {
-    showCircularMenu.value = !showCircularMenu.value;
+  showCircularMenu.value = !showCircularMenu.value;
 };
 
 const helpBot = ref(null);
@@ -83,22 +76,22 @@ const showBot = () => helpBot.value.open();
 
 const profileMenu = ref(null);
 const showProfileMenu = () => {
-    profileMenu.value.open();
+  profileMenu.value.open();
 };
 const profileMenuStatus = ref(false);
 provide("profileMenu", profileMenuStatus);
 const isProfileMenuOpenFn = (data) => {
-    profileMenuStatus.value = data;
-    provide("profileMenu", profileMenuStatus);
+  profileMenuStatus.value = data;
+  provide("profileMenu", profileMenuStatus);
 };
 
 const globalSearch = ref("");
 const globalSearchModal = ref(null);
 
 watchEffect(() => {
-    if (globalSearch.value) {
-        globalSearchModal.value.open();
-    }
+  if (globalSearch.value) {
+    globalSearchModal.value.open();
+  }
 });
 
 const isLoggedIn = ref(false);
@@ -106,11 +99,11 @@ const route = useRoute();
 const authCookie = useCookie("token", { watch: true });
 
 watchEffect(() => {
-    if (route.path === "/login") {
-        isLoggedIn.value = false;
-    } else {
-        isLoggedIn.value = true;
-    }
+  if (route.path === "/login") {
+    isLoggedIn.value = false;
+  } else {
+    isLoggedIn.value = true;
+  }
 });
 // watch(route, (newRoute) => {
 //   console.log({ route: route.value, newRoute: newRoute.value });
