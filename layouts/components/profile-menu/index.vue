f6836583e408a2527c7754be0d256e2357fc6e61
Chrys Ugwu <chrys@capeandbay.com>
2023-03-01 19:14:47 -0600

    Resolves #182

--- layouts/components/profile-menu/index.vue
+++ layouts/components/profile-menu/index.vue
@@ -105,7 +105,7 @@ const menu = [
   },
 ];
 
-const authToken = useCookie("token");
+const authToken = useCookie("auth");
 
 const handleClickLogout = () => {
   authToken.value = null;
---
+++ pages/Kiosk.vue
@@ -0,0 +1,19 @@
+<template>
+  111
+  <UserAddMenu ref="userMenu" mode="page"/>
+</template>
+
+<script setup>
+import UserAddMenu from "~/layouts/components/header-actions/user-add-menu.vue";
+import {ref} from "vue";
+
+const userMenu = ref(null);
+onMounted(() => {
+  // console.log(userMenu.value);
+  // userMenu.value.open();
+})
+</script>
+
+<style scoped>
+
+</style>
