b8e6f4af80c132ea521e6eeb6c4f9d7e23da00b5
Mobashir Monim <19724076+Mobashir-Monim@users.noreply.github.com>
2023-03-05 02:09:37 +0600

    Work on calendar view

--- pages/calendar/components/partials/calendar-menu.vue
+++ pages/calendar/components/partials/calendar-menu.vue
@@ -1,66 +1,70 @@
 <template>
     <!-- <ul> -->
     <!-- list header -->
-    <div class="flex justify-between items-center">
-        <h3 class="text-[1.2rem] font-semibold">
-            Calendars ({{ calendars.length }})
-        </h3>
+    <div class="flex flex-col gap-3">
+        <div class="flex justify-between items-center">
+            <h3 class="text-[1.2rem] font-semibold">
+                Calendars ({{ calendars.length }})
+            </h3>
 
-        <!-- button group -->
-        <div class="flex gap-2">
-            <!-- chevron btn -->
-            <button class="fill-white p-2 w-8 rotate-180">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                    <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                    <path
-                        d="M238 82.7l22.6 22.6 192 192L475.3 320 430 365.3l-22.6-22.6L238 173.3 68.6 342.6 46 365.3 .7 320l22.6-22.6 192-192L238 82.7z"
-                    />
-                </svg>
-            </button>
-            <!-- actions button (new/subscribe) -->
-            <div class="relative">
-                <button
-                    @click="enableDropdown"
-                    class="fill-white border-secondary hover:bg-secondary border-2 p-2 w-8 rounded-md"
-                >
+            <!-- button group -->
+            <div class="flex gap-2">
+                <!-- chevron btn -->
+                <button class="fill-white p-2 w-8 rotate-180">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 448 512"
+                        viewBox="0 0 512 512"
                     >
                         <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
                         <path
-                            d="M240 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H176V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H384c17.7 0 32-14.3 32-32s-14.3-32-32-32H240V80z"
+                            d="M238 82.7l22.6 22.6 192 192L475.3 320 430 365.3l-22.6-22.6L238 173.3 68.6 342.6 46 365.3 .7 320l22.6-22.6 192-192L238 82.7z"
                         />
                     </svg>
                 </button>
-                <template v-if="dropdownVisibility">
-                    <DropdownMenu @outclick="disableDropdown" />
-                </template>
+                <!-- actions button (new/subscribe) -->
+                <div class="relative">
+                    <button
+                        @click="enableDropdown"
+                        class="fill-white border-secondary hover:bg-secondary border-2 p-2 w-8 rounded-md"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 448 512"
+                        >
+                            <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                            <path
+                                d="M240 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H176V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H384c17.7 0 32-14.3 32-32s-14.3-32-32-32H240V80z"
+                            />
+                        </svg>
+                    </button>
+                    <template v-if="dropdownVisibility">
+                        <DropdownMenu @outclick="disableDropdown" />
+                    </template>
+                </div>
             </div>
         </div>
-    </div>
 
-    <!-- list items (calendars) -->
-    <ul class="max-h-[10vh] overflow-y-scroll">
-        <li
-            class="flex items-center justify-between my-1 pr-5"
-            v-for="cal in calendars"
-            :key="cal.id"
-        >
-            <div class="flex items-center gap-2">
-                <input
-                    type="checkbox"
-                    :name="cal.title"
-                    :id="'cal_' + cal.id"
-                />
-                <label
-                    class="!text-[0.9rem] !font-normal"
-                    :for="'cal_' + cal.id"
-                    >{{ cal.title }}</label
-                >
-            </div>
-            <div class="flex items-center gap-4">
-                <!-- <button class="w-3 fill-gray-300">
+        <!-- list items (calendars) -->
+        <ul class="max-h-[10vh] overflow-y-scroll">
+            <li
+                class="flex items-center justify-between my-1 pr-5"
+                v-for="cal in calendars"
+                :key="cal.id"
+            >
+                <div class="flex items-center gap-2">
+                    <input
+                        type="checkbox"
+                        :name="cal.title"
+                        :id="'cal_' + cal.id"
+                    />
+                    <label
+                        class="!text-[0.9rem] !font-normal"
+                        :for="'cal_' + cal.id"
+                        >{{ cal.title }}</label
+                    >
+                </div>
+                <div class="flex items-center gap-4">
+                    <!-- <button class="w-3 fill-gray-300">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 320 512"
@@ -70,20 +74,21 @@
                         />
                     </svg>
                 </button> -->
-                <button class="w-[0.3125rem] fill-gray-300">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 128 512"
-                    >
-                        <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                        <path
-                            d="M48 464a48 48 0 1 1 0-96 48 48 0 1 1 0 96zm0-160a48 48 0 1 1 0-96 48 48 0 1 1 0 96zM0 96a48 48 0 1 1 96 0A48 48 0 1 1 0 96z"
-                        />
-                    </svg>
-                </button>
-            </div>
-        </li>
-    </ul>
+                    <button class="w-[0.3125rem] fill-gray-300">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 128 512"
+                        >
+                            <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                            <path
+                                d="M48 464a48 48 0 1 1 0-96 48 48 0 1 1 0 96zm0-160a48 48 0 1 1 0-96 48 48 0 1 1 0 96zM0 96a48 48 0 1 1 96 0A48 48 0 1 1 0 96z"
+                            />
+                        </svg>
+                    </button>
+                </div>
+            </li>
+        </ul>
+    </div>
     <!-- </ul> -->
 </template>
 
--- pages/calendar/index.vue
+++ pages/calendar/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="py-4 px-12 w-full h-fit">
+    <div class="py-4 px-8 w-full h-fit">
         <!-- modal -->
         <daisy-modal class="w-fit" id="eventModal" ref="eventModal">
             <event-popup></event-popup>
@@ -12,11 +12,11 @@
             <EventDetails />
         </div>
         <!-- more in depth event information -->
-        <div
+        <!-- <div
             class="z-50 fixed h-screen w-screen flex items-center justify-end right-0 top-16"
         >
             <EventInformation />
-        </div>
+        </div> -->
 
         <!-- sidebar date selector popup -->
         <!-- <daisy-modal
@@ -103,7 +103,7 @@
                         />
                     </div>
                 </div>
-                <FullCalendar :options="calendarOptions" ref="calendar" />
+                <GrCalendar />
             </section>
         </div>
     </div>
@@ -118,9 +118,9 @@ import ReportsStatistics from "./components/partials/reports-statistics.vue";
 import EventDetails from "./components/partials/event-details.vue";
 import EventInformation from "./components/partials/event-information.vue";
 import Datepicker from "@vuepic/vue-datepicker";
+import GrCalendar from "./components/gr-calendar.vue";
 
 /** FullCalendar component & plugins */
-import FullCalendar from "@fullcalendar/vue3";
 import EventPopup from "./components/event-popup.vue";
 import listPlugin from "@fullcalendar/list";
 import dayGridPlugin from "@fullcalendar/daygrid";
@@ -132,7 +132,7 @@ import { calendarEvents as events } from "./helpers/calendar-events";
 const calenderView = ref("timeGridWeek");
 
 const handleDateClick = (arg) => {
-  console.log("date click! " + arg.dateStr);
+    console.log("date click! " + arg.dateStr);
 };
 
 /** DOM References */
@@ -154,57 +154,38 @@ const eventDetails = ref(null); // selected event information we need to pass in
 const eventDetailsVisibibility = ref(false);
 
 const handleChangeView = (value) => {
-  calenderView.value = value;
-  console.log("calenderView", calenderView.value);
-  calendar.value.getApi().changeView(value);
-  onViewChanged();
+    calenderView.value = value;
+    console.log("calenderView", calenderView.value);
+    calendar.value.getApi().changeView(value);
+    onViewChanged();
 };
 
 const eventClick = (info) => {
-  var eventObj = info.event;
-  eventModal.value.open();
+    var eventObj = info.event;
+    eventModal.value.open();
 };
 
 const calendarOptions = ref({
-  plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
-  schedulerLicenseKey: "0157232768-fcs-1652392378",
-  initialView: "timeGridWeek",
-  slotDuration: "01:00",
-  dateClick: handleDateClick,
-  headerToolbar: {
-    left: "",
-    center: "prev,today,next timeGridWeek,dayGridMonth,timeGridDay",
-    right: "",
-  },
-  events,
-  editable: true,
-  selectable: true,
-  dayMaxEvents: true,
-  eventClick,
-  datesSet: (params) => {
-    listCalendar?.value?.getApi()?.gotoDate(params.start);
-    monthCalendar?.value?.getApi()?.gotoDate(params.start);
-    monthCalendar?.value?.getApi()?.select(params.start);
-    //console.log("view-->",monthCalendar?.value?.getApi()?.view.getCurrentData().currentDate)
-  },
-  timeAxis: {
-    slotDuration: "01:00:00",
-  },
-  views: {
-    timeGridDay: {
-      dayHeaderFormat: {
-        month: "long",
-        day: "numeric",
-        omitCommas: "false",
-      },
-      nowIndicator: true,
+    plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+    schedulerLicenseKey: "0157232768-fcs-1652392378",
+    initialView: "timeGridWeek",
+    slotDuration: "01:00",
+    dateClick: handleDateClick,
+    headerToolbar: {
+        left: "",
+        center: "prev,today,next timeGridWeek,dayGridMonth,timeGridDay",
+        right: "",
     },
-    timeGridWeek: {
-      dayHeaderFormat: {
-        month: "short",
-        day: "numeric",
-      },
-      nowIndicator: true,
+    events,
+    editable: true,
+    selectable: true,
+    dayMaxEvents: true,
+    eventClick,
+    datesSet: (params) => {
+        listCalendar?.value?.getApi()?.gotoDate(params.start);
+        monthCalendar?.value?.getApi()?.gotoDate(params.start);
+        monthCalendar?.value?.getApi()?.select(params.start);
+        //console.log("view-->",monthCalendar?.value?.getApi()?.view.getCurrentData().currentDate)
     },
     timeAxis: {
         slotDuration: "01:00:00",
@@ -221,265 +202,126 @@ const calendarOptions = ref({
         timeGridWeek: {
             dayHeaderFormat: {
                 month: "short",
-                day: "2-digit",
+                day: "numeric",
             },
             nowIndicator: true,
         },
+        timeAxis: {
+            slotDuration: "01:00:00",
+        },
+        views: {
+            timeGridDay: {
+                dayHeaderFormat: {
+                    month: "long",
+                    day: "numeric",
+                    omitCommas: "false",
+                },
+                nowIndicator: true,
+            },
+            timeGridWeek: {
+                dayHeaderFormat: {
+                    month: "short",
+                    day: "2-digit",
+                },
+                nowIndicator: true,
+            },
+        },
+        viewDidMount: function (info) {
+            onViewChanged();
+        },
+        //eventContent: { html: '<i>some html</i>' }
     },
-    viewDidMount: function (info) {
-        onViewChanged();
-    },
-    //eventContent: { html: '<i>some html</i>' }
 });
 
 const monthCalendarOptions = ref({
-  plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
-  initialView: "dayGridMonth",
-  headerToolbar: false,
-  events,
-  eventClick,
-  editable: true,
-  selectable: true,
-  dayMaxEvents: true,
-  dateClick: function (params) {
-    console.log("🚀 ~ file: index.vue ~ line 123 ~ params", params);
-    calendar?.value?.getApi()?.gotoDate(params.date);
-  },
+    plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+    initialView: "dayGridMonth",
+    headerToolbar: false,
+    events,
+    eventClick,
+    editable: true,
+    selectable: true,
+    dayMaxEvents: true,
+    dateClick: function (params) {
+        console.log("🚀 ~ file: index.vue ~ line 123 ~ params", params);
+        calendar?.value?.getApi()?.gotoDate(params.date);
+    },
 });
 
 const listCalendarOptions = ref({
-  plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin],
-  initialView: "listDay",
-  timeAxis: {
-    slotDuration: "01:00:00",
-  },
-  headerToolbar: false,
-  events,
-  eventClick,
+    plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin],
+    initialView: "listDay",
+    timeAxis: {
+        slotDuration: "01:00:00",
+    },
+    headerToolbar: false,
+    events,
+    eventClick,
 });
 
 const onViewChanged = () => {
-  start.value = calendar.value.getApi().view.activeStart;
-  start.end = calendar.value.getApi().view.activeEnd;
-  console.log({
-    start,
-    end,
-    calendarView: calendar.value.getApi().view,
-  });
+    start.value = calendar.value.getApi().view.activeStart;
+    start.end = calendar.value.getApi().view.activeEnd;
+    console.log({
+        start,
+        end,
+        calendarView: calendar.value.getApi().view,
+    });
 };
 
 const calendarTitle = computed(() => {
-  let option = {
-    year: "numeric",
-    month: "long",
-  };
-  if (currentView.value === "timeGridDay") {
-    option["day"] = "numeric";
-  }
-  return start.value?.toLocaleString("default", option);
+    let option = {
+        year: "numeric",
+        month: "long",
+    };
+    if (currentView.value === "timeGridDay") {
+        option["day"] = "numeric";
+    }
+    return start.value?.toLocaleString("default", option);
 });
 const dateSelect = ref();
 const showDateSelectModal = () => {
-  if (calenderView.value !== "timeGridWeek") {
-    selectedDate.value = start.value;
-  }
-  dateSelect.value.open();
+    if (calenderView.value !== "timeGridWeek") {
+        selectedDate.value = start.value;
+    }
+    dateSelect.value.open();
 };
 const onSelectDate = (modelData) => {
-  selectedDate.value = modelData;
-  if (calenderView.value !== "timeGridWeek") {
-    start.value = modelData;
-    calendar.value.getApi().gotoDate(start.value);
-  } else {
-    start.value = modelData[0];
-    calendar.value.getApi().gotoDate(start.value);
-  }
+    selectedDate.value = modelData;
+    if (calenderView.value !== "timeGridWeek") {
+        start.value = modelData;
+        calendar.value.getApi().gotoDate(start.value);
+    } else {
+        start.value = modelData[0];
+        calendar.value.getApi().gotoDate(start.value);
+    }
 };
 
 const getDayClass = (date) => {
-  if (
-    isEqual(
-      date,
-      set(new Date(), {
-        hours: 0,
-        minutes: 0,
-        seconds: 0,
-        milliseconds: 0,
-      })
+    if (
+        isEqual(
+            date,
+            set(new Date(), {
+                hours: 0,
+                minutes: 0,
+                seconds: 0,
+                milliseconds: 0,
+            })
+        )
     )
-  )
-    return "!rounded-full bg-secondary";
-  return "";
+        return "!rounded-full bg-secondary";
+    return "";
 };
 
 watch(calenderView, () => {
-  setTimeout(() => {
-    calendar.value.getApi().render();
-    console.log("huh");
-  });
+    setTimeout(() => {
+        calendar.value.getApi().render();
+        console.log("huh");
+    });
 });
 
 onMounted(async () => {
-  await nextTick();
-  window.dispatchEvent(new Event("resize"));
+    await nextTick();
+    window.dispatchEvent(new Event("resize"));
 });
 </script>
-
-<style>
-.calendar-date-picker {
-    .dp__theme_dark {
-        --dp-background-color: #000000;
-        --dp-menu-border-color: #191919;
-    }
-    .dp__calendar_header {
-        @apply text-secondary;
-    }
-    .dp__month_year_row {
-        @apply border-none;
-        .dp__month_year_col_nav {
-            @apply bg-secondary rounded;
-            .dp__inner_nav {
-                @apply text-base-content;
-                &:hover {
-                    @apply bg-secondary;
-                }
-            }
-        }
-      }
-    }
-    .dp__calendar {
-        @apply bg-transparent text-[0.8rem];
-        .dp__calendar_wrap {
-            @apply bg-neutral border border-secondary rounded;
-            .dp__calendar_header_separator {
-                @apply h-0;
-            }
-        }
-    }
-  }
-}
-
-.dp__cell_inner {
-    @apply !p-0 !h-[25px];
-}
-
-.dp__month_year_select {
-    @apply flex-row !py-0 !h-auto;
-}
-
-.dp__month_year_select {
-    @apply flex-row !py-0 !h-auto;
-}
-
-.fc .fc-list-sticky .fc-list-day > *,
-.fc-theme-standard .fc-list-day-cushion,
-.fc .fc-cell-shaded,
-.fc-theme-standard th,
-.fc .fc-list-event:hover td {
-  background-color: transparent !important;
-}
-.fc-button {
-  @apply !bg-transparent !border-0 !rounded-md !py-[2px] !capitalize !text-sm transition-all duration-150 ease-linear;
-}
-/* fc-dayGridMonth-button fc-button fc-button-primary */
-.fc-button.fc-button-active {
-  @apply !bg-secondary !border-[1px] !border-[#073A76];
-}
-.fc-toolbar-chunk {
-  @apply !flex !flex-row !gap-5;
-}
-.fc-toolbar-chunk .fc-button-group {
-  @apply bg-[#18203A] border-[2px] border-[#073A76] p-[6px] rounded-md;
-}
-.fc-prev-button.fc-button.fc-button-primary,
-.fc-today-button.fc-button.fc-button-primary,
-.fc-next-button.fc-button.fc-button-primary {
-  @apply !bg-secondary;
-}
-.fc-button-group {
-  @apply !flex !flex-row !gap-2;
-}
-.fc-prev-button,
-.fc-next-button {
-  @apply !px-[2px];
-}
-.fc-event-main {
-  @apply !border-l-8 border-white border-opacity-20 pl-2;
-}
-
-.fc {
-  @apply w-full;
-}
-
-.fc-media-screen {
-  @apply min-h-[55rem];
-}
-
-th.fc-day-today {
-    @apply !bg-[#191919]/[0.4];
-}
-
-.fc-event-main-frame {
-  @apply !pl-2;
-}
-
-.fc .fc-scrollgrid-section.fc-scrollgrid-section-body:first-child {
-  * {
-    @apply border-b-secondary;
-  }
-}
-.fc-col-header-cell.fc-day {
-  height: 4em;
-  @apply bg-gradient-to-b from-secondary/80 via-secondary to-secondary/60;
-  div {
-    @apply h-full flex items-center justify-center uppercase;
-  }
-}
-
-.fc .fc-scrollgrid-section:nth-child(2) {
-  @apply bg-secondary;
-  * {
-    @apply border-b-secondary;
-  }
-}
-.fc-theme-standard td:hover {
-  @apply cursor-pointer;
-}
-.fc .fc-list-event:hover td {
-  background-color: rgba(188, 232, 241, 0.3) !important;
-  @apply cursor-pointer;
-}
-.month-calender .fc-daygrid-day-bottom a {
-  font-size: 0px;
-  height: 10px;
-  width: 10px;
-}
-.fc .fc-timegrid-slot {
-  height: 3.5em;
-}
-.fc .fc-day-today {
-  @apply bg-blend-darken;
-}
-.fc-col-header-cell-cushion {
-    @apply !font-normal !capitalize;
-}
-
-.fc-timegrid-axis-cushion {
-    @apply capitalize text-[0.7rem];
-}
-
-.fc-header-toolbar.fc-toolbar.fc-toolbar-ltr {
-    @apply !my-4;
-}
-.fc-scrollgrid.fc-scrollgrid-liquid {
-    @apply !max-h-[66vh] !overflow-scroll;
-}
-.fc-timegrid th,
-.fc-timegrid td {
-    @apply !text-[0.8rem];
-}
-.fc-timegrid,
-.fc-view-harness {
-    @apply !max-h-[66vh];
-}
-</style>
