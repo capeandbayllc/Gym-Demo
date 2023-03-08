53b81aaad821a03684a600108a81e950b1aaee58
Chrys Ugwu <chrys@capeandbay.com>
2023-03-08 14:26:38 -0600

    Resolves #227

--- pages/people-search/components/people-search-table-row.vue
+++ pages/people-search/components/people-search-table-row.vue
@@ -1,5 +1,5 @@
 <template>
-    <tr class="people-lead-tbl-row">
+    <tr class="people-lead-tbl-row text-sm">
         <td>{{data.segment}}</td>
         <td>
             <div class="flex items-center">
@@ -13,7 +13,7 @@
         <td>
             <membership-btn :membership="data.type"/>
         </td>
-        <td>{{data.created}}</td>
+        <td>{{ data.created.replace(/:\d{2}(AM|PM)$/, ' $1') }}</td>
     </tr>
 </template>
 <style scoped>
