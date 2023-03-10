1037caed1b6df5612ff054c312b5d94cd3da5fad
CatsMeow492 <taylormohneytech@gmail.com>
2023-03-10 13:22:38 -0800

    built assessment section in fitness page

---
+++ pages/fitness/components/fitness-card-title.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="fitness-card-title">
+        <div class="flex items-center text-white">
+            <div class="title-icon">
+                <component :is="titleIcon" />
+            </div>
+            {{title}}
+        </div>
+    </div>
+</template>
+<style scoped>
+.fitness-card-title {
+    @apply flex bg-secondary justify-between h-12 items-center text-lg font-semibold px-[26px] py-7;
+    .title-icon {
+        @apply flex items-center justify-center w-9 h-9 mr-3 bg-neutral rounded-[14px];
+    }
+}
+</style>
+<script setup>
+const props = defineProps({
+    titleIcon: Object,
+    title: String
+})
+</script>
---
+++ pages/fitness/components/waivers-card/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <fitness-card
+        :title-icon="WaiversIcon"
+        title="Signed Waivers"
+    >
+        
+    </fitness-card>
+</template>
+<style scoped>
+
+</style>
+<script setup>
+import FitnessCard from '../fitness-card.vue'
+</script>
