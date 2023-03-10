ec4ea747fc1f5929708bfe16dcc892a90621b2f3
CatsMeow492 <taylormohneytech@gmail.com>
2023-03-10 13:44:35 -0800

    updated fitness/index.vue file

--- pages/fitness/index.vue
+++ pages/fitness/index.vue
@@ -1,14 +1,25 @@
 <template>
     <div class="page-fitness-container">
-        <div class="page-content">TBD</div>
+        <div class="page-content grid grid-cols-1 gap-5">
+            <assessments-card/>
+            <waivers-card />
+            <questionnaires-card />
+            <reportings-card />
+
+        </div>
     </div>
 </template>
 <style scoped>
 .page-fitness-container {
     @apply py-4 pr-5 w-full h-fit;
     .page-content {
-        @apply text-center text-5xl text-secondary ;
+        @apply text-center text-secondary p-6 border-[#1b70b1] border-2 rounded-[] bg-[#054171] ;
     }
 }
 </style>
-<script setup></script>
+<script setup>
+import AssessmentsCard from './components/assessments-card/index.vue';
+import WaiversCard from './components/waivers-card/index.vue';
+import ReportingsCard from './components/reportings-card/index.vue';
+import QuestionnairesCard from './components/questionnaires-card/index.vue';
+</script>
