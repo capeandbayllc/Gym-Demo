7b58536e7e73c4bec4532fb13c69d197c36252c4
David Duncan <devduncan89@gmail.com>
2023-03-10 12:30:57 -0500

    check-in button animations & simplify event

--- pages/check-in/profile-card/check-in-button.vue
+++ pages/check-in/profile-card/check-in-button.vue
@@ -1,47 +1,38 @@
 <template>
+  <button
+    @click="$emit('click')"
+    class="relative z-[1] w-full max-h-[4rem] h-full rounded-2xl border-2 border-secondary group"
+  >
+    <!-- green back -->
     <div
-        class="alert-container transition-all duration-150 ease-linear"
-        :class="{
-            checked: modelValue,
-        }"
-        @click="$emit('update:modelValue', !modelValue)"
-    >
-        <div
-            class="checker"
-            :class="{
-                checked: modelValue,
-            }"
-        >
-            <check-circle-icon class="checkin-icon" />
-        </div>
-        <div class="text-xs my-auto">CHECK IN</div>
+      class="bg-lime-500 absolute h-full w-full -z-[1] top-0 left-0 right-0 bottom-0 rounded-2xl"
+    ></div>
+
+    <!-- blue back -->
+    <div
+      class="bg-secondary rounded-2xl absolute h-full w-full -z-[0] scale-x-0 group-hover:scale-x-100 transition-all duration-300 top-0 left-0 right-0 bottom-0"
+    ></div>
+
+    <div class="relative">
+      <span
+        class="h-6 w-6 absolute top-1/2 -translate-y-1/2 left-0 transition-all duration-500 translate-x-8 group-hover:translate-x-36 group-hover:fill-white"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+          <path
+            d="M243.8 339.8C232.9 350.7 215.1 350.7 204.2 339.8L140.2 275.8C129.3 264.9 129.3 247.1 140.2 236.2C151.1 225.3 168.9 225.3 179.8 236.2L224 280.4L332.2 172.2C343.1 161.3 360.9 161.3 371.8 172.2C382.7 183.1 382.7 200.9 371.8 211.8L243.8 339.8zM512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z"
+          />
+        </svg>
+      </span>
+
+      <span
+        class="text-black text-2xl uppercase font-semibold whitespace-nowrap group-hover:text-white -translate-x-12 group-hover:-translate-x-20 right-0 absolute top-1/2 -translate-y-1/2 transition-all duration-500"
+        >Check in</span
+      >
     </div>
+  </button>
 </template>
-<style scoped>
-.alert-container {
-    @apply flex flex-row w-fit items-center gap-3 mx-auto h-8 px-2 py-1 cursor-pointer rounded-full bg-accent-focus text-base-300;
-    .checker {
-        @apply rounded-full overflow-hidden transition-all duration-150 ease-linear;
-        .checkin-icon {
-            @apply border-2 border-base-300 rounded-full text-transparent transition-all duration-150 ease-linear;
-        }
-    }
-    &:hover, &.checked {
-       @apply flex-row-reverse text-base-content bg-secondary;
-        .checker {
-            .checkin-icon {
-               @apply border-base-content;
-               :deep(path) {
-                 stroke: white !important;
-               }
-            }
-        }
-    }
-}
-</style>
+
 <script setup>
-import { PlusCircleIcon, CheckCircleIcon } from "~~/components/icons";
-const props = defineProps({
-    modelValue: Boolean,
-});
+const emit = defineEmits(["click"]);
 </script>
--- pages/check-in/profile-card/profile-options.vue
+++ pages/check-in/profile-card/profile-options.vue
@@ -1,32 +1,32 @@
 <template>
-    <div class="profile-options-container relative">
-        <div class="flex flex-row gap-2 relative">
-            <CheckInButton v-model="checkIn" class="text-base-content" />
-        </div>
-        <div class="profile-options card-gradient-bg">
-            <profile-option-item
-                v-for="item in options"
-                :key="item.key"
-                :model-value="item.key === selected"
-                @update:modelValue="handleSelect(item.key, $event)"
-                :label="item.label"
-                :unread="item.unread"
-            />
-        </div>
+  <div class="profile-options-container relative">
+    <!-- <div class="flex flex-row gap-2 relative"> -->
+    <CheckInButton @click="handleCheckIn" />
+    <!-- </div> -->
+    <div class="profile-options card-gradient-bg">
+      <profile-option-item
+        v-for="item in options"
+        :key="item.key"
+        :model-value="item.key === selected"
+        @update:modelValue="handleSelect(item.key, $event)"
+        :label="item.label"
+        :unread="item.unread"
+      />
     </div>
+  </div>
 </template>
 <style scoped>
 .profile-options-container {
-    @apply flex flex-col space-y-6 items-center;
-    .profile-options {
-        @apply flex flex-col min-w-[220px] justify-between mt-5 text-xs h-[500px] border-2 border-secondary rounded-lg p-5 w-full bg-secondary/20;
-    }
-    .btn-close-selection {
-        @apply cursor-pointer bg-base-content rounded p-2 text-sm text-black;
-    }
-    .btn-check-in {
-        @apply px-4 flex items-center rounded cursor-pointer bg-accent-focus/60 uppercase text-black font-medium;
-    }
+  @apply flex flex-col space-y-6 items-center;
+  .profile-options {
+    @apply flex flex-col min-w-[220px] justify-between mt-5 text-xs h-[500px] border-2 border-secondary rounded-lg p-5 w-full bg-secondary/20;
+  }
+  .btn-close-selection {
+    @apply cursor-pointer bg-base-content rounded p-2 text-sm text-black;
+  }
+  .btn-check-in {
+    @apply px-4 flex items-center rounded cursor-pointer bg-accent-focus/60 uppercase text-black font-medium;
+  }
 }
 </style>
 <script setup>
@@ -34,12 +34,12 @@ import { ref } from "vue";
 import ProfileOptionItem from "./profile-option-item.vue";
 import CheckInButton from "./check-in-button.vue";
 
-const emit = defineEmits(['update:selected', 'update:check-in'])
+const emit = defineEmits(["update:selected", "update:check-in"]);
 
 const props = defineProps({
-    selected: String,
+  selected: String,
 });
-const user = useState('auth');
+const user = useState("auth");
 const notificationCount = ref(user.value.notifications.length);
 
 const options = reactive([
@@ -83,17 +83,23 @@ const options = reactive([
 
 const checkIn = ref(false);
 
-watch(() => user.value.notifications, (notifications) => {
-  notificationCount.value = notifications.length;
-});
+const handleCheckIn = () => {
+  checkIn.value = !checkIn;
+  emit("update:check-in", checkIn);
+};
 
-watch(checkIn, (value) => emit('update:check-in', value));
+watch(
+  () => user.value.notifications,
+  (notifications) => {
+    notificationCount.value = notifications.length;
+  }
+);
 
 const handleSelect = (key, value) => {
-    if (value) {
-        emit("update:selected", key);
-    } else {
-        emit("update:selected", null);
-    }
+  if (value) {
+    emit("update:selected", key);
+  } else {
+    emit("update:selected", null);
+  }
 };
 </script>
