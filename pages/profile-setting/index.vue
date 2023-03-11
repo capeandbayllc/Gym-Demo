62783b49e99185b83e9d2d58fe8d84010344d023
Piash <nazem.piash10@gmail.com>
2023-03-11 10:02:08 +0600

    feature/nazem.piash/settings-page-and-link-to-profile-menu commit description: 1. add user image icon 2. design the inner page content border 3. dynamic form for basic profile info at the top section

--- pages/profile-setting/index.vue
+++ pages/profile-setting/index.vue
@@ -1,138 +1,138 @@
 <template>
   <div class="page-setting-container">
-    <div class="page-content">
-      <div
-          class="col-span-3 3xl:col-span-4 -lg:col-span-2 -md:col-span-1 relative flex justify-center"
-      >
-        <img
-            src="/account-lg.png"
-            class="w-32 cursor-pointer"
-            @click="uploadPopUp = true"
-        />
-        <div
-            v-if="uploadPopUp"
-            class="bg-white absolute p-4 w-[300px] text-right rounded border border-secondary"
-        >
-          <label class="mb-4 block">
-            <UploadIcon
-                class="bg-secondary rounded-full text-center relative z-10 text-secondary block mx-auto cursor-pointer"
-            />
-            <input
-                type="file"
-                class="absolute z-0 top-0 left-0 hidden"
-            />
-          </label>
-          <Button
-              ghost
-              size="sm"
-              class="text-black"
-              @click="uploadPopUp = false"
-          >Cancel
-          </Button
-          >
-          <Button
-              secondary
-              size="sm"
-              @click="uploadPopUp = false"
-          >Save
-          </Button
-          >
+    <div class="col-span-3 3xl:col-span-4 -lg:col-span-2 -md:col-span-1 relative flex justify-center">
+      <img
+          src="/user_profile_page_icon.png"
+          class="w-60 cursor-pointer"
+          @click="uploadPopUp = true"
+      />
+<!--      <div-->
+<!--          v-if="uploadPopUp"-->
+<!--          class="bg-white absolute p-4 w-[300px] text-right rounded border border-secondary"-->
+<!--      >-->
+<!--        <label class="mb-4 block">-->
+<!--          <UploadIcon-->
+<!--              class="bg-secondary rounded-full text-center relative z-10 text-secondary block mx-auto cursor-pointer"-->
+<!--          />-->
+<!--          <input-->
+<!--              type="file"-->
+<!--              class="absolute z-0 top-0 left-0 hidden"-->
+<!--          />-->
+<!--        </label>-->
+<!--        <Button-->
+<!--            ghost-->
+<!--            size="sm"-->
+<!--            class="text-black"-->
+<!--            @click="uploadPopUp = false"-->
+<!--        >Cancel-->
+<!--        </Button>-->
+<!--        <Button-->
+<!--            secondary-->
+<!--            size="sm"-->
+<!--            @click="uploadPopUp = false"-->
+<!--        >Save-->
+<!--        </Button-->
+<!--        >-->
+<!--      </div>-->
+    </div>
+
+<!--    <div class="bg-gradient-to-b from-purple-500 to-blue-500 p-4 rounded-lg mt-10">-->
+<!--    </div>-->
+
+    <div class="mx-auto mt-10 gradient-bg p-4 page-border w-5/6 ">
+      <div class="bg-black page-border">
+        <div class="grid grid-cols-1 py-2">
+            <div class="grid grid-cols-3 justify-items-center py-2">
+              <div v-for="item in profileBasic" :key="item.key">
+                <div>{{ item.label }}</div>
+                <input :class="item.class"
+                       v-model="form[item.key]"
+                       :placeholder="item.placeholder"/>
+              </div>
+            </div>
         </div>
       </div>
 
-      <template
-          v-for="section in sections"
-          :key="section.key"
-      >
-        <div
-            v-for="item in section.data"
-            :key="item.key"
-            :class="`col-span-${
-                        item.span ? item.span : 1
-                    } -lg:col-span-1 -md:col-auto`"
-        >
-          <div>{{ item.label }}</div>
-          <input
-              :class="item.class"
-              v-model="form[item.key]"
-              :placeholder="item.placeholder"
-          />
-        </div>
-        <div class="divider"></div>
-      </template>
-      <profile-relation/>
-      <div class="divider"></div>
-      <div>
-        <h4 class="mb-4 inline-block">
-          Calendar Access
-          <LockIcon class="inline-block"/>
-        </h4>
-        <multi-select
-            :options="departments"
-            label="Department(s)"
-            v-model="form.departments"
-        />
-      </div>
-      <div class="col-span-2 3xl:col-span-3 -lg:col-span-1 -md:col-auto">
-        <div
-            class="border border-secondary rounded p-4 w-full mb-4 bg-transparent"
-        >
-          <h4 class="mb-1 inline-block font-bold w-full">
-            Availability
-          </h4>
-          <label class="mr-3">Daily Times Set:</label>
-          <span class="mx-2 inline-block">
-                        <ClockIcon class="inline-block mr-1"/> 10:20 am
-                    </span>
-          <span class="mx-2 inline-block">
-                        <ClockIcon class="inline-block mr-1"/> 1:00 pm
-                    </span>
-          <span class="mx-2 inline-block">
-                        <ClockIcon class="inline-block mr-1"/> 3:00 pm
-                    </span>
-        </div>
-        <Button
-            secondary
-            size="sm"
-            class="float-right capitalize"
-        >+ New
-        </Button
-        >
-        <Button
-            ghost
-            size="sm"
-            class="float-right mr-2 capitalize"
-        >Edit
-        </Button
-        >
-      </div>
-      <div class="divider"></div>
-      <div
-          class="col-span-3 3xl:col-span-4 -lg:col-span-2 -md:col-span-1"
-      >
-        <h4 class="mb-4 inline-block w-full">Notes</h4>
-        <textarea
-            name="notes"
-            rows="6"
-            class="neutral-input rounded w-full mb-4"
-        ></textarea>
-        <div class="text-center">
-          <Button
-              ghost
-              size="sm"
-              class="mr-2 capitalize"
-          >Cancel
-          </Button
-          >
-          <Button
-              secondary
-              size="sm"
-              class="capitalize"
-          >Save
-          </Button
-          >
-        </div>
-      </div>
+      <!--&lt;!&ndash;              <div class="divider"></div>&ndash;&gt;-->
+
+
+<!--          <profile-relation/>-->
+<!--          <div class="divider"></div>-->
+<!--          <div>-->
+<!--            <h4 class="mb-4 inline-block">-->
+<!--              Calendar Access-->
+<!--              <LockIcon class="inline-block"/>-->
+<!--            </h4>-->
+<!--            <multi-select-->
+<!--                :options="departments"-->
+<!--                label="Department(s)"-->
+<!--                v-model="form.departments"-->
+<!--            />-->
+<!--          </div>-->
+<!--          <div class="col-span-2 3xl:col-span-3 -lg:col-span-1 -md:col-auto">-->
+<!--            <div-->
+<!--                class="border border-secondary rounded p-4 w-full mb-4 bg-transparent"-->
+<!--            >-->
+<!--              <h4 class="mb-1 inline-block font-bold w-full">-->
+<!--                Availability-->
+<!--              </h4>-->
+<!--              <label class="mr-3">Daily Times Set:</label>-->
+<!--              <span class="mx-2 inline-block">-->
+<!--                        <ClockIcon class="inline-block mr-1"/> 10:20 am-->
+<!--                    </span>-->
+<!--              <span class="mx-2 inline-block">-->
+<!--                        <ClockIcon class="inline-block mr-1"/> 1:00 pm-->
+<!--                    </span>-->
+<!--              <span class="mx-2 inline-block">-->
+<!--                        <ClockIcon class="inline-block mr-1"/> 3:00 pm-->
+<!--                    </span>-->
+<!--            </div>-->
+<!--            <Button-->
+<!--                secondary-->
+<!--                size="sm"-->
+<!--                class="float-right capitalize"-->
+<!--            >+ New-->
+<!--            </Button-->
+<!--            >-->
+<!--            <Button-->
+<!--                ghost-->
+<!--                size="sm"-->
+<!--                class="float-right mr-2 capitalize"-->
+<!--            >Edit-->
+<!--            </Button-->
+<!--            >-->
+<!--          </div>-->
+<!--          <div class="divider"></div>-->
+<!--          <div-->
+<!--              class="col-span-3 3xl:col-span-4 -lg:col-span-2 -md:col-span-1"-->
+<!--          >-->
+<!--            <h4 class="mb-4 inline-block w-full">Notes</h4>-->
+<!--            <textarea-->
+<!--                name="notes"-->
+<!--                rows="6"-->
+<!--                class="neutral-input rounded w-full mb-4"-->
+<!--            ></textarea>-->
+<!--            <div class="text-center">-->
+<!--              <Button-->
+<!--                  ghost-->
+<!--                  size="sm"-->
+<!--                  class="mr-2 capitalize"-->
+<!--              >Cancel-->
+<!--              </Button-->
+<!--              >-->
+<!--              <Button-->
+<!--                  secondary-->
+<!--                  size="sm"-->
+<!--                  class="capitalize"-->
+<!--              >Save-->
+<!--              </Button-->
+<!--              >-->
+<!--            </div>-->
+<!--          </div>-->
+
+<!--        </div>-->
+<!--      </div>-->
+
     </div>
   </div>
 </template>
@@ -149,8 +149,8 @@ export default {
   @apply py-4 w-full h-fit;
 }
 
-.page-content {
-  @apply bg-neutral border border-secondary rounded grid grid-cols-3 3xl:grid-cols-4 -lg:grid-cols-2 -md:grid-cols-1 gap-x-12 gap-y-8 px-8 py-10;
+.page-border {
+  @apply border border-secondary rounded-2xl;
 }
 
 input {
@@ -187,68 +187,79 @@ input:focus {
 
 <script setup>
 import {ref} from 'vue';
-import ProfileRelation from './components/profile-relation.vue';
+import ProfilefRelation from './components/profile-relation.vue';
 import {ClockIcon, LockIcon, UploadIcon} from '~~/components/icons';
 
 const form = ref({
   username: 'kevinbuchanan@email.com',
 });
-const profile = [
+const profileBasic = [
   {
     key: 'first_name',
     label: 'First Name',
-    class: 'secondary-input',
+    class: 'neutral-input',
+    required: true
   },
   {
     key: 'middle_name',
     label: 'Middle Name',
-    class: 'secondary-input',
+    class: 'neutral-input',
+    required: false,
   },
   {
     key: 'last_name',
     label: 'Last Name',
-    class: 'secondary-input',
+    class: 'neutral-input',
+    required: false,
   },
   {
     key: 'birthday',
     label: 'Date of Birth',
     class: 'neutral-input',
+    required: false,
   },
   {
     key: 'gender',
     label: 'Gender',
     class: 'neutral-input',
+    required: false,
   },
   {
     key: 'username',
     label: 'Username',
     class: 'readonly-input',
+    required: false,
   },
   {
     key: 'email',
     label: 'Email',
     class: 'neutral-input',
+    required: false,
   },
   {
     key: 'altenrate_email',
     label: 'Personal Email',
     class: 'neutral-input',
+    required: false,
   },
   {
     key: 'phone',
     label: 'Mobile',
     class: 'neutral-input',
+    required: false,
   },
   {
     key: 'employee_id',
     label: 'Employee I.D. Number',
     class: 'neutral-input',
+    required: false,
   },
   {
     key: 'manager',
     label: 'Manager',
     class: 'outline-input',
     placeholder: 'Session Manager',
+    required: false,
   },
 ];
 
@@ -278,13 +289,13 @@ const location = [
 
 const sections = [
   {
-    key: 'profile',
-    data: profile,
+    key: 'profileBasic',
+    data: profileBasic,
   },
-  {
-    key: 'location',
-    data: location,
-  },
+  // {
+  //   key: 'location',
+  //   data: location,
+  // },
 ];
 const departments = [
   {
