ed7799c137e4e5137f23d8301ce638e2ced7d8c3
David Duncan <devduncan89@gmail.com>
2023-03-13 14:10:02 -0400

    updated modal classes to make the design possible to implement

--- layouts/components/pulse-modal/index.vue
+++ layouts/components/pulse-modal/index.vue
@@ -1,27 +1,30 @@
 <template>
-	<div class="pulse-modal-border">
+  <!-- <div class="pulse-modal-border">
 		<div class="pulse-modal-container">
 			<pulse-summary />
 			<div class="pulse-icon">
 				<img src="/pulse-logo.svg" alt="" class="w-20 h-5">
 			</div>
 		</div>
-	</div>
+	</div> -->
+  <div>
+    <pulse-summary />
+  </div>
 </template>
 <style scoped>
-.pulse-modal-border {
-	@apply border-8 rounded-lg border-secondary w-full;
-	box-shadow: 0 0 16px 4px hsl(var(--s, var(--nc)));
+/* .pulse-modal-border {
+  @apply border-8 rounded-lg border-secondary w-full;
+  box-shadow: 0 0 16px 4px hsl(var(--s, var(--nc)));
 }
 .pulse-modal-container {
-	@apply flex flex-col p-5 xl:p-10 border-4 rounded-lg shadow-lg justify-center bg-neutral space-y-10;
-	margin: -3px;
+  @apply flex flex-col p-5 xl:p-10 border-4 rounded-lg shadow-lg justify-center bg-neutral space-y-10;
+  margin: -3px;
 }
 .pulse-icon {
-	@apply  flex justify-center;
-	transform: scale(2);
-}
+  @apply flex justify-center;
+  transform: scale(2);
+} */
 </style>
 <script setup>
-import PulseSummary from './pulse-summary.vue'
+import PulseSummary from "./pulse-summary.vue";
 </script>
--- layouts/components/pulse-modal/pulse-summary.vue
+++ layouts/components/pulse-modal/pulse-summary.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="pulse-summary">
-		<div class="pulse-summary-item">
+  <div class="pulse-summary">
+    <!-- <div class="pulse-summary-item">
 			<h4>New Members</h4>
 			<pulse-member-summary />
 		</div>
@@ -12,22 +12,56 @@
 		<div class="pulse-summary-item">
 			<h4>Check In History</h4>
 			<pulse-check-history />
-		</div>
-	</div>
+		</div> -->
+    <!-- scrolling side container (no container title, component titles) -->
+    <section class="max-h-[50vh] h-full flex">
+      <div class="overflow-y-scroll">
+        <ul>
+          <li>
+            <h4>Club Membership Details</h4>
+          </li>
+          <li>
+            <h4>Membership Draft Breakdown</h4>
+          </li>
+          <li>
+            <h4>Memberhsip Sales Leaderboard</h4>
+          </li>
+          <li>
+            <h4>Membership Calls and Mass Comms</h4>
+          </li>
+          <li>
+            <h4>Personal Training Details</h4>
+          </li>
+          <li>
+            <h4>Personal Training Draft Breakdown</h4>
+          </li>
+          <li>
+            <h4>Personal Training Sales Leaderboard</h4>
+          </li>
+          <li v-for="item in Array(25)" :key="item">
+            <h4>Personal Training Events</h4>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h4>Club History & details</h4>
+      </div>
+    </section>
+  </div>
 </template>
 <style scoped>
-.pulse-summary {
-	@apply flex flex-col xl:flex-row xl:space-x-5 -xl:space-y-5 overflow-y-auto max-h-[100vw];
+/* .pulse-summary {
+  @apply flex flex-col xl:flex-row xl:space-x-5 -xl:space-y-5 overflow-y-auto max-h-[100vw];
 }
 .pulse-summary-item {
-	@apply flex flex-col space-y-3;
-}
+  @apply flex flex-col space-y-3;
+} */
 h4 {
-	@apply text-xl font-semibold;
+  @apply text-xl font-semibold;
 }
 </style>
 <script setup>
-import PulseMemberSummary from './pulse-member-summary.vue'
-import PulseClubEvents from './pulse-club-events.vue'
-import PulseCheckHistory from './pulse-check-history.vue'
+import PulseMemberSummary from "./pulse-member-summary.vue";
+import PulseClubEvents from "./pulse-club-events.vue";
+import PulseCheckHistory from "./pulse-check-history.vue";
 </script>
