bf121d87b4c70616da781dc4dd4a37203c6a831d
Philip Krogel <philip@kro.dev>
2023-03-13 10:43:32 -0400

    replace query selector with refs

--- pages/check-in/index.vue
+++ pages/check-in/index.vue
@@ -1,96 +1,87 @@
 <template>
-    <div class="w-full h-full flex flex-col check-in-modal-height">
-        <div class="page-checkin-container">
-            <div class="page-content">
-                <div
-                    class="wrapper px-2 pt-2 w-full rounded-md"
-                    :class="{ 'pb-0': detailView !== null }"
-                >
-                    <div class="flex -md:block">
-                        <event-card class="mr-2" />
-                        <profile-card
-                            :active-option="detailView"
-                            @select-option="detailView = $event"
-                            @toggle-detail="toggleDetailSection"
-                            @check-in="OnCheckInStatusChange"
-                            @open-engage="showEngageModal"
-                        />
-                    </div>
-                    <div class="bg-secondary w-full">
-                        <p
-                            class="checkin-card-detail"
-                            @click="toggleDetailSection"
-                        >
-                            <span>
-                                {{ isDetailOpened ? "Close" : "View Account" }}
-                            </span>
-                            <LockIcon
-                                class="inline-block"
-                                v-if="!isDetailOpened"
-                            />
-                            <UnlockIcon class="inline-block" v-else />
-                        </p>
-                        <div class="account-box" v-if="isDetailOpened">
-                            <ul>
-                                <ii
-                                    v-for="{ key, icon } in accountOptions"
-                                    :key="key"
-                                >
-                                    <button @click="changeDetailView(key)">
-                                        <component :is="icon" />
-                                    </button>
-                                </ii>
-                            </ul>
-                            <UserInfo v-if="detailView == 'memberinfo'" />
-                            <Setting v-if="detailView == 'setting'" />
-                            <button
-                                v-if="detailView !== null"
-                                class="mx-auto p-4 w-full"
-                                @click="backToTop"
-                            >
-                                Back to Top
-                            </button>
-                        </div>
-                    </div>
-                </div>
+  <div class="w-full h-full flex flex-col check-in-modal-height">
+    <div class="page-checkin-container" ref="checkInContainer">
+      <div class="page-content">
+        <div
+          class="wrapper px-2 pt-2 w-full rounded-md"
+          :class="{ 'pb-0': detailView !== null }"
+        >
+          <div class="flex -md:block">
+            <event-card class="mr-2" />
+            <profile-card
+              :active-option="detailView"
+              @select-option="detailView = $event"
+              @toggle-detail="toggleDetailSection"
+              @check-in="OnCheckInStatusChange"
+              @open-engage="showEngageModal"
+            />
+          </div>
+          <div class="bg-secondary w-full">
+            <p class="checkin-card-detail" @click="toggleDetailSection">
+              <span>
+                {{ isDetailOpened ? "Close" : "View Account" }}
+              </span>
+              <LockIcon class="inline-block" v-if="!isDetailOpened" />
+              <UnlockIcon class="inline-block" v-else />
+            </p>
+            <div class="account-box" v-if="isDetailOpened">
+              <ul>
+                <ii v-for="{ key, icon } in accountOptions" :key="key">
+                  <button @click="changeDetailView(key)">
+                    <component :is="icon" />
+                  </button>
+                </ii>
+              </ul>
+              <UserInfo v-if="detailView == 'memberinfo'" />
+              <Setting v-if="detailView == 'setting'" />
+              <button
+                v-if="detailView !== null"
+                class="mx-auto p-4 w-full"
+                @click="backToTop"
+              >
+                Back to Top
+              </button>
             </div>
+          </div>
         </div>
-        <div class="page-content">
-            <template v-for="{ key, component } in subSections" :key="key">
-                <component
-                    v-if="detailView === key"
-                    @close="detailView = null"
-                    :key="key"
-                    :is="component"
-                />
-            </template>
-        </div>
+      </div>
     </div>
+    <div class="page-content">
+      <template v-for="{ key, component } in subSections" :key="key">
+        <component
+          v-if="detailView === key"
+          @close="detailView = null"
+          :key="key"
+          :is="component"
+        />
+      </template>
+    </div>
+  </div>
 </template>
 <style scoped>
 .page-checkin-container {
-    @apply py-4 px-5 w-full h-fit;
-    .page-title {
-        @apply text-lg font-light pb-3 pl-5;
+  @apply py-4 px-5 w-full h-fit;
+  .page-title {
+    @apply text-lg font-light pb-3 pl-5;
+  }
+  .page-content {
+    @apply flex flex-col-reverse md:flex-row gap-5 flex-wrap justify-center;
+    .wrapper {
+      background: #0074c8;
     }
-    .page-content {
-        @apply flex flex-col-reverse md:flex-row gap-5 flex-wrap justify-center;
-        .wrapper {
-            background: #0074c8;
-        }
-        .account-box {
-            ul {
-                @apply flex w-10/12 justify-around mx-auto -md:w-full;
+    .account-box {
+      ul {
+        @apply flex w-10/12 justify-around mx-auto -md:w-full;
 
-                li {
-                    @apply inline-block;
-                }
-            }
+        li {
+          @apply inline-block;
         }
-        .checkin-card-detail {
-            @apply text-center cursor-pointer align-middle my-2 flex flex-row items-center justify-center space-x-3;
-        }
+      }
     }
+    .checkin-card-detail {
+      @apply text-center cursor-pointer align-middle my-2 flex flex-row items-center justify-center space-x-3;
+    }
+  }
 }
 </style>
 <script setup>
@@ -106,90 +97,90 @@ import GuestCard from "./guest-card/index.vue";
 import NoteCard from "./note-card/index.vue";
 import NewAgreement from "./new-agreement/index.vue";
 import {
-    LockIcon,
-    MemberInfoIcon,
-    SettingIcon,
-    DollarDocIcon,
-    AnnouncementIcon,
-    PieChartIcon,
-    BookIcon,
-    DocIcon,
-    UnlockIcon,
+  LockIcon,
+  MemberInfoIcon,
+  SettingIcon,
+  DollarDocIcon,
+  AnnouncementIcon,
+  PieChartIcon,
+  BookIcon,
+  DocIcon,
+  UnlockIcon,
 } from "~~/components/icons";
 import UserInfo from "./user-info/index.vue";
 import Setting from "./setting/index.vue";
 import Engage from "./engage/index.vue";
 const option = ref(null);
 
+const appLayout = useLayoutElement();
+const checkInContainer = ref();
+
 watch(option, () => {
-    const appLayout = document.querySelector(".app-layout");
-    const scrollTO =
-        document.querySelector(".page-checkin-container").offsetHeight + 100;
-    setTimeout(() => {
-        appLayout.scroll({
-            top: scrollTO,
-            left: 0,
-            behavior: "smooth",
-        });
-    }, 500);
+  const scrollTO = checkInContainer.value.offsetHeight + 100;
+  setTimeout(() => {
+    appLayout.value.scroll({
+      top: scrollTO,
+      left: 0,
+      behavior: "smooth",
+    });
+  }, 500);
 });
 
 const showEngageModal = () => {
-    detailView.value = "engage";
+  detailView.value = "engage";
 };
 
 const isDetailOpened = ref(false);
 const detailView = ref(null);
 
 const changeDetailView = (view) => {
-    if (view == detailView.value) {
-        detailView.value = null;
-    } else {
-        detailView.value = view;
-    }
+  if (view == detailView.value) {
+    detailView.value = null;
+  } else {
+    detailView.value = view;
+  }
 };
 
 const backToTop = () => {
-    const appLayout = document.querySelector(".app-layout");
-    appLayout.scroll({
-        top: 0,
-        left: 0,
-        behavior: "smooth",
-    });
+  appLayout.value.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
 };
 
 const subSections = [
-    { key: "profile", component: Profile },
-    { key: "pos", component: PosWindow },
-    { key: "calendar", component: CalendarCard },
-    { key: "notification", component: NotificationCard },
-    { key: "guest-pass", component: GuestCard },
-    { key: "note", component: NoteCard },
-    { key: "newAgreement", component: NewAgreement },
-    { key: "engage", component: Engage },
+  { key: "profile", component: Profile },
+  { key: "pos", component: PosWindow },
+  { key: "calendar", component: CalendarCard },
+  { key: "notification", component: NotificationCard },
+  { key: "guest-pass", component: GuestCard },
+  { key: "note", component: NoteCard },
+  { key: "newAgreement", component: NewAgreement },
+  { key: "engage", component: Engage },
 ];
 
 const accountOptions = [
-    { key: "memberinfo", icon: MemberInfoIcon },
-    { key: "setting", icon: SettingIcon },
-    { key: "#", icon: DollarDocIcon },
-    { key: "#", icon: AnnouncementIcon },
-    { key: "#", icon: PieChartIcon },
-    { key: "#", icon: BookIcon },
-    { key: "#", icon: DocIcon },
+  { key: "memberinfo", icon: MemberInfoIcon },
+  { key: "setting", icon: SettingIcon },
+  { key: "#", icon: DollarDocIcon },
+  { key: "#", icon: AnnouncementIcon },
+  { key: "#", icon: PieChartIcon },
+  { key: "#", icon: BookIcon },
+  { key: "#", icon: DocIcon },
 ];
 
 const toggleDetailSection = (event) => {
-    if (typeof event != "boolean") {
-        isDetailOpened.value = !isDetailOpened.value;
-        detailView.value = null;
-    } else {
-        isDetailOpened.value = event;
-        detailView.value = null;
-    }
+  if (typeof event != "boolean") {
+    isDetailOpened.value = !isDetailOpened.value;
+    detailView.value = null;
+  } else {
+    isDetailOpened.value = event;
+    detailView.value = null;
+  }
 };
 
 function OnCheckInStatusChange(isCheckedIn) {
-    toggleDetailSection(isCheckedIn);
+  toggleDetailSection(isCheckedIn);
 }
 </script>
