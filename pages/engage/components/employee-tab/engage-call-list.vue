52ec710bea037b67fc30d9028edb64606d729aa3
David Duncan <devduncan89@gmail.com>
2023-03-13 16:00:45 -0400

    format data

--- pages/engage/components/employee-tab/engage-call-list.vue
+++ pages/engage/components/employee-tab/engage-call-list.vue
@@ -1,30 +1,29 @@
 <template>
-    <div class="engage-call-list-container">
-        <!-- <h3>Call List</h3> -->
-        <h3 class="text-secondary">Upgrades:</h3>
+  <div class="engage-call-list-container">
+    <!-- <h3>Call List</h3> -->
+    <h3 class="text-secondary">Upgrades:</h3>
 
-        <data-table
-            :columns="columns"
-            :data="itemsWithMembershipTypes"
-            :row-component="CallListItem"
-            :stickyHeader="true"
-            class="h-80 text-xs overflow-y-auto"
-        />
-
-    </div>
+    <data-table
+      :columns="columns"
+      :data="members2"
+      :row-component="CallListItem"
+      :stickyHeader="true"
+      class="h-80 text-xs overflow-y-auto"
+    />
+  </div>
 </template>
 <style scoped>
 .engage-call-list-container {
-    @apply col-span-2 pb-1 -md:col-span-3;
-    h3 {
-        @apply text-base;
-    }
+  @apply col-span-2 pb-1 -md:col-span-3;
+  h3 {
+    @apply text-base;
+  }
 }
 </style>
 <script setup>
-import CallListItem from './call-list-item.vue';
+import CallListItem from "./call-list-item.vue";
 import gql from "graphql-tag";
-import {useQuery} from "@vue/apollo-composable";
+import { useQuery } from "@vue/apollo-composable";
 
 const query = gql`
   query AllMembers {
@@ -40,7 +39,7 @@ const query = gql`
           name
         }
         member_type {
-            name
+          name
         }
         homeLocation {
           name
@@ -57,80 +56,98 @@ const query = gql`
   }
 `;
 
-
-
 const { result } = useQuery(query);
 
 const members = ref([]);
+
 watch(result, (nv, ov) => {
   if (result) {
-    members.value = result._value.members.data;
+    console.log("result", result);
+    members.value = result.value.members.data;
   }
-})
+});
 
-const membershipTypes = ['platinum', 'gold', 'silver', 'bronze'];
+const membershipTypes = ["platinum", "gold", "silver", "bronze"];
 
 const getRandomMembershipType = () => {
   return membershipTypes[Math.floor(Math.random() * membershipTypes.length)];
 };
 
-const itemsWithMembershipTypes = members.value.map(item => ({
-  ...item,
-  membership_type: getRandomMembershipType()
-}));
+const members2 = computed(() => {
+  return members.value.map((item) => {
+    return {
+      ...item,
+      membership_type: getRandomMembershipType(),
+    };
+  });
+});
 
-
-console.log('test');
+console.log("test");
 console.log(members);
 
-const columns = ["","Name", "Location", "", "Membership Type", "Date Upgraded", ""]
-const data = [{
+const columns = [
+  "",
+  "Name",
+  "Location",
+  "",
+  "Membership Type",
+  "Date Upgraded",
+  "",
+];
+const data = [
+  {
     id: 1,
     avatar: "/account.png",
     name: "Sandy Lin",
     location: "Tru Fit Ware Road",
     date: "1.02.22",
     membership: "silver",
-    call_type: 'Call'
-}, {
+    call_type: "Call",
+  },
+  {
     id: 2,
     avatar: "/account.png",
     name: "Randy Portis",
     location: "Tru Fit San Angelo",
     date: "1.02.22",
     membership: "gold",
-    call_type: 'Email'
-}, {
+    call_type: "Email",
+  },
+  {
     id: 3,
     avatar: "/account.png",
     name: "Tess Hightower",
     location: "Tru Fit University",
     date: "1.02.22",
     membership: "platinum",
-    call_type: 'In-Club'
-}, {
+    call_type: "In-Club",
+  },
+  {
     id: 4,
     avatar: "/account.png",
     name: "Sandy Lin",
     location: "Tru Fit Lubbock 4Th Street",
     date: "1.02.22",
     membership: "bronze",
-    call_type: 'Call'
-}, {
+    call_type: "Call",
+  },
+  {
     id: 5,
     avatar: "/account.png",
     name: "Randy Portis",
     location: "Tru Fit Sa Bandera",
     date: "1.02.22",
     membership: "platinum",
-    call_type: 'Email'
-}, {
+    call_type: "Email",
+  },
+  {
     id: 6,
     avatar: "/account.png",
     name: "Tess Hightower",
     location: "Tru Fit Harlingen",
     date: "1.02.22",
     membership: "gold",
-    call_type: 'In-Club'
-}]
+    call_type: "In-Club",
+  },
+];
 </script>
