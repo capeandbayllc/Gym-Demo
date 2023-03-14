ddde8c968d27ddaeb705bcc6a062494015a84ee8
CatsMeow492 <taylormohneytech@gmail.com>
2023-03-14 12:54:39 -0700

    Added Reporting and KPIs section

--- pages/fitness/components/reportings-card/index.vue
+++ pages/fitness/components/reportings-card/index.vue
@@ -1,14 +1,21 @@
 <template>
-    <fitness-card
-        :title-icon="ReportingIcon"
-        title="Reporting and KPIs"
-    >
-        
-    </fitness-card>
+	<fitness-card :title-icon="ReportingIcon" title="Reporting and KPIs">
+		<div class="grid grid-cols-3 gap-5 p-6">
+			<IncomeChart class="col-span-1"/>
+			<PercentageRadar class="col-span-1"/>
+			<UnitsSoldChart class="col-span-1"/>
+			<!-- <DraftReportingChart class="col-span-3"/> -->
+			<!-- <SessionReportingList class="col-span-3" /> -->
+		</div> 
+	</fitness-card>
 </template>
 <style scoped>
-
 </style>
 <script setup>
-import FitnessCard from '../fitness-card.vue'
+import FitnessCard from '../fitness-card.vue';
+import IncomeChart from './income-chart.vue';
+import PercentageRadar from './percentage-radar.vue';
+import UnitsSoldChart from './units-sold-chart.vue';
+import DraftReportingChart from './draft-reporting-chart.vue';
+import SessionReportingList from './session-reporting.vue';
 </script>
---
+++ pages/fitness/components/reportings-card/percentage-radar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="percentage-container">
+        <label class="header block text-[14px]">Percentages</label>
+        <div class="percent-item grid grid-cols-10">
+            <PercentPie class="col-span-3"/>
+            <div class="statistics col-span-5"></div>
+            <div class="percent-button"></div>
+        </div>
+        <div class="percent-item"></div>
+        <div class="percent-item"></div>
+    </div>
+</template>
+<style scoped>
+.percentage-container{
+    padding: 10px 20px;
+    border : 2px solid #0077ac;
+    border-radius: 15px;
+}
+.header{
+    color : white;
+    text-align: left;
+}
+</style>
+<script setup>
+import PercentPie from './percent-pie.vue';
+</script>
