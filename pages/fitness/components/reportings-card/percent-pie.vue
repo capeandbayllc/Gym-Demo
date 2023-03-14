8cc8e88fac0930f39ee3ab1e4d7630f9bf1deb30
CatsMeow492 <taylormohneytech@gmail.com>
2023-03-14 12:54:39 -0700

    Added Reporting and KPIs section

---
+++ pages/fitness/components/reportings-card/percent-pie.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <ClientOnly>
+            <apexchart
+                type="radialBar"
+                :options="options"
+                width="200"
+                height="200"
+            />
+        </ClientOnly>
+    </div>
+</template>
+<script setup>
+
+var options = {
+  chart: {
+    height: 280,
+    type: "radialBar"
+  },
+  
+  series: [67],
+  
+  plotOptions: {
+    radialBar: {
+      hollow: {
+        margin: 15,
+        size: "70%"
+      },
+     
+      dataLabels: {
+        showOn: "always",
+        name: {
+          offsetY: -10,
+          show: true,
+          color: "#888",
+          fontSize: "13px"
+        },
+        value: {
+          color: "#111",
+          fontSize: "30px",
+          show: true
+        }
+      }
+    }
+  },
+
+  stroke: {
+    lineCap: "round",
+  },
+  labels: ["Progress"]
+};
+
+</script>
---
+++ pages/fitness/components/reportings-card/units-sold-chart.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="units-sold">
+        <div class="grid grid-cols-10 header">
+            <label class="col-span-2 text-[12px] text-white">Units Sold</label>
+            <div class="col-span-5"></div>
+            <div class="col-span-3 grid grid-cols-5">
+                <div class="col-span-1 flex">
+                    <button class="btn-status -rotate-90">></button>
+                </div>
+                <div class="col-span-4 grid grid-cols-1 ml-1">
+                    <span class="col-span-1 text-white text-[12px] text-left">Last Week</span>
+                    <span class="col-span-1 text-[14px] text-left"> +0.10%</span>
+                </div>
+            </div>
+        </div>
+        <ClientOnly class="col-span-5">
+            <apexchart 
+                type="area"
+                :options="options"
+                :series="series"
+            />
+        </ClientOnly>
+    </div>
+</template>
+<style scoped>
+.btn-status{
+    width: 15px;
+    height: 15px;
+    background : #0077ac;
+    border-radius: 50%;
+    color: black;
+    font-size: 10px;
+    font-weight: bold;
+    margin : auto;
+}
+.units-sold{
+    border : 2px solid #0077ac;
+    border-radius : 15px;
+    padding: 10px;
+}
+.header{
+    margin-top: 5px;
+    margin-bottom: 10px;
+}
+.apexcharts-grid{
+    background: blue;
+}
+</style>
+<script setup>
+
+const series= [{
+    name: 'comming',
+    data: [12,68,25,48,11]
+    }];
+const options ={
+    chart:{
+        height : 350,
+        type: 'area',
+        toolbar:{
+            show: false,
+        },
+    },
+    dataLabels: {
+        enabled : false
+    },
+    stroke: {
+        curve: 'smooth',
+        width : 2,
+        colors : ['#FFF']
+    },
+    xaxis: {
+        type: 'category',
+        categories: ["Jan", "Feb", "Mar", "Apr", "May"],
+        tooltip : {
+            enabled: false
+        },
+        color: '#FFF',
+        labels:{
+            style: {
+                colors: '#FFF'
+            }
+        }
+    },
+    yaxis:{
+        show : false
+    },
+    grid: {
+        show : false
+    },
+    fill:{
+        
+        type: 'gradient',
+        gradient:{
+            shade : 'dark',
+            type: 'vertical',
+            shadeIntensity : 1,
+            gradientToColors: ['#0C1C31', '#0077ac'],
+            inverseColors : true,
+            opacityTo : 1,
+            opacityFrom : 1,
+        }
+    },
+    tooltip: {
+        enabled: true,
+        custom: function({series, seriesIndex, dataPointIndex, w}) {
+            return '<div class="arrow_box">' +
+            '<span>' + series[seriesIndex][dataPointIndex] + '</span>' +
+            '</div>'
+        }
+    },
+
+};
+</script>
