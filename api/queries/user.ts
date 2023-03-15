55c442d88b4ac1ccaa2b0144e7ff1d71d2c16c01
Philip Krogel <philip@kro.dev>
2023-03-15 10:37:16 -0400

    updateUser mutation

--- api/queries/user.ts
+++ api/queries/user.ts
@@ -1,30 +1,44 @@
 import gql from "graphql-tag";
-import {DocumentNode} from "graphql/language";
-import {GraphQLObject} from "~/api/queries/GraphQLObject";
+import { DocumentNode } from "graphql/language";
+import { GraphQLObject } from "~/api/queries/GraphQLObject";
 
 const FIND_BY_MAIL: DocumentNode = gql`
-    query User($email: String!) {
-        user(email: $email) {
-            id
-            first_name
-            last_name
-            email
-            profile_photo_path
-            phone
-        }
+  query User($email: String!) {
+    user(email: $email) {
+      id
+      first_name
+      last_name
+      email
+      profile_photo_path
+      phone
     }
+  }
+`;
+
+const UPDATE_USER = gql`
+  mutation UpdateUser($input: UserInput) {
+    updateUser(input: $input) {
+      id
+      first_name
+      last_name
+      email
+      profile_photo_path
+      phone
+    }
+  }
 `;
 
 export interface UserQuery {
-    findByMail: DocumentNode
+  findByMail: DocumentNode;
 }
 
 const user: GraphQLObject<UserQuery, object> = {
-    query: {
-        findByMail: FIND_BY_MAIL,
-    },
-    mutation: {
-    }
-}
+  query: {
+    findByMail: FIND_BY_MAIL,
+  },
+  mutation: {
+    updateUser: UPDATE_USER,
+  },
+};
 
 export default user;
