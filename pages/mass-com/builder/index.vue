c9557683c191c5b0f2e2264bda00822904928ca1
Sovon Saha <saha.sovon89@gmail.com>
2023-03-15 20:42:36 +0600

    Closes #244

---
+++ pages/mass-com/builder/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="campaign-builder-container">
+        <div class="page-topbar flex flex-1">
+            Campaign Builder
+        </div>
+
+        <div class="page-content card-gradient-bg">
+            <div class="page-content-block">
+                
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+.campaign-builder-container {
+    @apply pb-4 w-full h-fit pr-5;
+    .page-topbar {
+        @apply pl-10 pr-4 mb-2
+    }
+    .page-content {
+        @apply flex border border-secondary rounded-3xl min-h-[25rem] max-w-[45rem] mt-[4rem] p-3;
+        .page-content-block {
+            @apply flex-1 bg-black border border-secondary rounded-2xl min-h-fit;
+        }
+    }
+}
+</style>
+
+<script setup>
+
+const props = defineProps({
+    data: Object
+})
+
+</script>
