bf25befe36b16cdc86b933df82bc4d30118b746e
Abimael <abimaelc0618@gmail.com>
2023-03-16 18:23:02 -0300

    adding functionality to the filter checkboxes in the global search modal

--- pages/people-search/components/people-search-action.vue
+++ pages/people-search/components/people-search-action.vue
@@ -1,20 +1,4 @@
 <template>
-    <!-- <div class="flex flex-row justify-between space-x-4 mt-10 mb-4">
-        <div class="flex gap-6">
-            <Button size="sm" class="font-medium border-base-content text-base-content" :class="{'active' : activeBtn === item.type}" @click="setActiveAction(item.type)" v-for="(item, index) in actionsBtn" :key="index">
-                {{ item.name }}
-            </Button>
-        </div>
-        <div class="flex gap-6">
-            <select-box
-                :items="filterType"
-                value=""
-                :label="'Locations'" 
-                :secondary="true"
-                class="w-40">
-            </select-box>
-        </div>
-    </div> -->
     <div class="flex gap-4 flex-wrap xl:flex-nowrap mx-4 overflow-hidden">
         <div class="toggle-custom" :class="{ 'toggle-active': leadsCheck }">
             <div @click="changeLeadsCheck" class="circle"></div>
@@ -23,11 +7,11 @@
                 Leads
             </span>
         </div>
-        <div class="toggle-custom" :class="{ 'toggle-active': leads2Check }">
-            <div @click="changeLeads2Check" class="circle"></div>
-            <div @click="changeLeads2Check" class="line"></div>
-            <span @click="changeLeads2Check">
-                Leads
+        <div class="toggle-custom" :class="{ 'toggle-active': membersCheck }">
+            <div @click="changeMembersCheck" class="circle"></div>
+            <div @click="changeMembersCheck" class="line"></div>
+            <span @click="changeMembersCheck">
+                Members
             </span>
         </div>
         <div class="toggle-custom" :class="{ 'toggle-active': employeesCheck }">
@@ -44,7 +28,7 @@
                 Segments
             </span>
         </div>
-        <button class="text-gray-400" @click="clearChecks">
+        <button class="text-gray-400" @click="setActiveAction('');">
             Clear
         </button>
     </div>
@@ -119,30 +103,32 @@
 }
 </style>
 <script setup>
-const props = defineProps({
-    actionsBtn: Array,
-});
 const activeBtn = ref('');
 const emit = defineEmits(['people-search-action-selected'])
 
 
 const leadsCheck = ref(false);
 const changeLeadsCheck = ()=>{
+    setActiveAction('leads');
     leadsCheck.value = !leadsCheck.value;
 };
 
-const leads2Check = ref(false);
-const changeLeads2Check = ()=>{
-    leads2Check.value = !leads2Check.value;
+const membersCheck = ref(false);
+const changeMembersCheck = ()=>{
+    setActiveAction('members');
+    membersCheck.value = !membersCheck.value;
 };
 
 const employeesCheck = ref(false);
 const changeEmployeesCheck = ()=>{
+    // No work
+    // setActiveAction('employees');
     employeesCheck.value = !employeesCheck.value;
 };
 
 const segmentsCheck = ref(false);
 const changeSegmentsCheck = ()=>{
+    setActiveAction('segments');
     segmentsCheck.value = !segmentsCheck.value;
 };
 
@@ -156,26 +142,4 @@ const setActiveAction = (name) => {
         emit('people-search-action-selected', name);
     }
 }
-const filterType = [
-    {
-        value: "1",
-        label: "Location 1"
-    },
-    {
-        value: "2",
-        label: "Location 2"
-    },
-    {
-        value: "3",
-        label: "Location 3"
-    },
-    {
-        value: "4",
-        label: "Location 4"
-    },
-    {
-        value: "5",
-        label: "Location 5"
-    },
-];
 </script>
