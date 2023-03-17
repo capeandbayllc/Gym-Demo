ac9a51e4e81e155f0025f4887801b4c6a3f7d6c5
Abimael <abimaelc0618@gmail.com>
2023-03-17 17:32:23 -0300

    fix padding issue on communication col of the global search modal

--- pages/people-search/components/people-search-table-row.vue
+++ pages/people-search/components/people-search-table-row.vue
@@ -88,7 +88,7 @@
 </template>
 <style scoped>
 .people-lead-tbl-row {
-    @apply bg-base-100 rounded-[15px] h-14 flex items-center px-[20px] py-[30px] my-[13px] cursor-pointer transition-all duration-75 border-2 border-base-100 hover:border-secondary hover:bg-primary;
+    @apply bg-base-100 rounded-[15px] h-14 flex items-center pr-[20px] py-[30px] my-[13px] cursor-pointer transition-all duration-75 border-2 border-base-100 hover:border-secondary hover:bg-primary;
 
     td {
         @apply border-y-2 border-secondary text-left px-3;
