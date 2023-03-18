fe5d330f23352da6069214471d09c1c57ad6ead1
Mobashir Monim <19724076+Mobashir-Monim@users.noreply.github.com>
2023-03-19 00:23:13 +0600

    Remove redundant css classes

--- components/select-box-radio/select-box-content.vue
+++ components/select-box-radio/select-box-content.vue
@@ -7,13 +7,4 @@
 .select-box-content {
     @apply overflow-y-scroll cursor-pointer py-2 absolute z-20 left-0 bg-base-content text-neutral border rounded-b text-xs w-full scrollbar-hide max-h-32 overflow-y-scroll;
 }
-
-.scrollbar-hide::-webkit-scrollbar {
-    display: none;
-}
-
-.scrollbar-hide {
-    -ms-overflow-style: none;
-    scrollbar-width: none;
-}
 </style>
