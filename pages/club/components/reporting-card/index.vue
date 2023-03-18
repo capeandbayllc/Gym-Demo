6393cfe1a8ed11e10c47986646e5e9d2a13f9ebd
Mohammad Ashraufuddin Ferdousi <ashferd.business@gmail.com>
2023-03-18 19:39:23 +0600

    Reporting widged for clubhub.

--- pages/club/components/reporting-card/index.vue
+++ pages/club/components/reporting-card/index.vue
@@ -1,13 +1,15 @@
 <template>
     <club-card :title-icon="ReportingIcon" title="Reporting">
-sdffsd
+        <PercentageRadar />
     </club-card>
 </template>
 <script setup>
     import { ReportingIcon } from '~~/components/icons'
     import ClubCard from '../club-card.vue';
+    import PercentageRadar from './percentage-radar.vue';
     onMounted(async () => {
         await nextTick();
         window.dispatchEvent(new Event('resize'))
     });
+
 </script>
