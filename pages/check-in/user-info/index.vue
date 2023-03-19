679a463acb14352b755aa2f315cb8628038f79b5
blair <blair@capeandbay.com>
2023-03-19 19:53:46 -0400

    changes from meeting

--- pages/check-in/user-info/index.vue
+++ pages/check-in/user-info/index.vue
@@ -1,15 +1,15 @@
 <template>
     <MemberInformation/>
     <Demographics/>
-    <AccessCode/>
-    <BankingInformation/>
-    <EmployeeDetails/>
+<!--    <AccessCode/>-->
+<!--    <BankingInformation/>-->
+<!--    <EmployeeDetails/>-->
 </template>
 
 <script setup>
 import MemberInformation from './member-information.vue'
 import Demographics from './demographics.vue'
-import AccessCode from './access-code.vue'
-import BankingInformation from './banking-information.vue'
-import EmployeeDetails from './employee-details.vue'
+// import AccessCode from './access-code.vue'
+// import BankingInformation from './banking-information.vue'
+// import EmployeeDetails from './employee-details.vue'
 </script>
