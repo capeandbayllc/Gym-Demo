0a746484e045273acad58482ba81217ddc92752b
Mohammad Lummim Sarker <lamimsarker@gmail.com>
2023-03-20 15:07:29 +0600

    added lead icon and menu

---
+++ components/icons/addLead.vue
@@ -0,0 +1,12 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" width="38.348" height="38.348" viewBox="0 0 38.348 38.348">
+        <g id="Group_6870" data-name="Group 6870" transform="translate(-1253.283 -2728.932)">
+            <g id="Ellipse_1443" data-name="Ellipse 1443" transform="translate(1253.283 2728.932)" fill="none" stroke="#fff" stroke-width="2">
+            <circle cx="19.174" cy="19.174" r="19.174" stroke="none"/>
+            <circle cx="19.174" cy="19.174" r="18.174" fill="none"/>
+            </g>
+            <line id="Line_879" data-name="Line 879" y2="20.026" transform="translate(1272.631 2738.092)" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+            <line id="Line_880" data-name="Line 880" x1="18.748" transform="translate(1263.256 2748.318)" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+        </g>
+    </svg>
+</template>
