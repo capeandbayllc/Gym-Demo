bf577e4266ffea502fe93828812478f32a926c20
David Duncan <devduncan89@gmail.com>
2023-03-30 13:34:31 -0400

    fix overlapping bar thingy

--- pages/people-search/index.vue
+++ pages/people-search/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div
+    id="ppl-search"
     class="p-6 w-full max-w-7xl rounded-2xl border-2 border-secondary bg-gradient-to-b from-[#031222]/75 to-black mt-8"
   >
     <div>
@@ -14,6 +15,12 @@
     <search-list @row-clicked="rowClicked" :filter="tblFilter"></search-list>
   </div>
 </template>
+
+<style>
+.app-body-content:has(#ppl-search) {
+  @apply px-20;
+}
+</style>
 
 <script setup>
 import SearchList from "./components/search-list.vue";
