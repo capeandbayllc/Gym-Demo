5959153bb4aa4d911050f3dfa0100310dfd8e983
CatsMeow492 <taylormohneytech@gmail.com>
2023-03-30 16:33:41 -0400

    Updating the badge color for membership type

--- pages/mass-com/components/conversion-data/conversion-table-row.vue
+++ pages/mass-com/components/conversion-data/conversion-table-row.vue
@@ -1,12 +1,22 @@
 <template>
     <tr class="conversion-table-row">
         <td>
-            <img :src="data.src" alt="User Photo" height="40" width="40"/>
+            <img :src="data.src" alt="User Photo" height="40" width="40" />
         </td>
-        <td><div class="conversion-data">{{data.name}}</div></td>
-        <td><div class="conversion-data">{{data.unit}}</div></td>
-        <td><div class="conversion-data">{{data.membership}}</div></td>
-        <td><div class="conversion-data">{{data.revenue}}</div></td>
+        <td>
+            <div class="conversion-data">{{ data.name }}</div>
+        </td>
+        <td>
+            <div class="conversion-data">{{ data.unit }}</div>
+        </td>
+        <td>
+            <div class="conversion-data">
+                <membership-btn :membership="data.membership" />
+            </div>
+        </td>
+        <td>
+            <div class="conversion-data">{{ data.revenue }}</div>
+        </td>
     </tr>
 </template>
 <style>
@@ -24,12 +34,15 @@
         .conversion-title {
             @apply text-xl font-bold;
         }
+        @apply px-4;
     }
 }
 </style>
 
 <script setup>
+import MembershipBtn from "@/components/buttons/membership-btn.vue";
+
 const props = defineProps({
-    data: Object
-})
+    data: Object,
+});
 </script>
