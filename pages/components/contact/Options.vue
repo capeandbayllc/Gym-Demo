20432e80f007a360dca92734d25a1cd933ff330c
Abimael <abimaelc0618@gmail.com>
2023-04-17 18:47:07 -0300

    refactor engage->message component for reusability

--- pages/components/contact/Options.vue
+++ pages/components/contact/Options.vue
@@ -16,7 +16,7 @@
 <script setup lang="ts">
 import MakeCall from "~/pages/check-in/side-car-split/make-call.vue";
 import SendEmail from '~/pages/check-in/engage/email.vue';
-import SendSms from "~/pages/check-in/side-car-split/send-sms.vue";
+import SendSms from "~/pages/check-in/engage/message.vue";
 import OngoingCall from "~/pages/check-in/side-car-split/ongoing-call.vue";
 
 export type Type = 'text' | 'email' | 'call' | null;
