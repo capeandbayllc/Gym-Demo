9885f2edf2dc5b87c3b6fdb79a82af15dd00d4a4
Abimael <abimaelc0618@gmail.com>
2023-04-17 20:40:43 -0300

    add effective date and promo code component to check-in->agreement

--- pages/check-in/user-info/pay-now.vue
+++ pages/check-in/user-info/pay-now.vue
@@ -33,38 +33,7 @@
                 <p class="text-xs px-2 mb-5 font-light">Annual fee will be billed on January 20th. <br/> Taxes may apply.</p>
             </div>
           </div>
-          <div class="flex-1 mt-12">
-            <div class="bg-gradient-to-b from-secondary to-[#003255] rounded-t-[14px] text-center py-1">
-                <p class="font-light text-sm -mb-1">Effective Date</p>
-                <p class="text-xl font-semibold">22 Dec 2022</p>
-            </div>
-            <div class="px-12 py-3">
-            <div class="mb-2 w-full flex">
-                <p class="w-9/12">Enrollment Fee</p>
-                <span class="text-secondary font-semibold w-3/12"> $0.00</span>
-            </div>
-            <div class="mb-2 w-full flex">
-                <p class="w-9/12">This Month's Dues</p>
-                <span class="text-secondary font-semibold w-3/12"> $0.00</span>
-            </div>
-            <div class="mb-2 w-full flex">
-                <p class="w-9/12">Taxes</p>
-                <span class="text-secondary font-semibold w-3/12"> $0.00</span>
-            </div>
-            <div class="mb-2 w-full flex">
-                <p class="w-9/12">Original Total Due</p>
-                <span class="text-secondary font-semibold w-3/12"> $18.96</span>
-            </div>
-            <div class="mb-2 font-semibold w-full flex">
-                <p class="text-secondary w-9/12">Total Due Today</p>
-                <span class="text-secondary font-semibold w-3/12">$9.48</span>
-            </div>
-            </div>
-            <div class="bg-gradient-to-b from-secondary to-[#003255] rounded-b-[14px] text-center py-3">
-                <p class="font-light text-sm -mb-1">PROMO CODE</p>
-                <p class="text-xl font-semibold">XMAS DAY <span class="ml-10">-$9.48</span></p>
-            </div>
-          </div>
+          <EffectiveDateAndPricing class="mt-12" />
         </div>
         <div class="flex items-center justify-around my-4">
           <div class="flex items-center mr-6">
@@ -99,6 +68,7 @@
 </template>
 <script setup>
 import {ref} from 'vue';
+import EffectiveDateAndPricing from './components/EffectiveDateAndPricing.vue';
 
 const props = defineProps({
     modalClass:{
