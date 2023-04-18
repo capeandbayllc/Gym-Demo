181d28010e52f093a313b602442b7a84226fb618
Abimael <abimaelc0618@gmail.com>
2023-04-18 20:10:35 -0300

    refactor to reuse charts/area component

--- components/charts/area/index.vue
+++ components/charts/area/index.vue
@@ -1,22 +1,22 @@
 <template>
-    <div class="income-chart flex flex-col justify-between">
+    <div class="chart-area flex flex-col justify-between">
         <div class="grid grid-cols-10 header">
-            <label class="col-span-2 text-[12px] text-white">Income</label>
+            <label class="col-span-2 text-[12px] text-white">{{ title }}</label>
             <div class="col-span-5"></div>
             <div class="col-span-3 grid grid-cols-5">
                 <div class="col-span-1 flex">
-                    <button class="btn-status rotate-90">></button>
+                    <button class="btn-status" :class="lastWeek < 0 ?'rotate-90':'-rotate-90'">></button>
                 </div>
                 <div class="col-span-4 grid grid-cols-1 ml-1">
                     <span class="col-span-1 text-white text-[12px] text-left">Last Week</span>
-                    <span class="col-span-1 text-[14px] text-left"> -0.7%</span>
+                    <span class="col-span-1 text-[14px] text-left"> {{ (lastWeek < 0 ? "-" : "+") + Math.abs(lastWeek).toFixed(2) }}%</span>
                 </div>
             </div>
         </div>
         <ClientOnly class="col-span-5">
-            <apexchart
-                class="income-apex-chart"
+            <apexchart 
                 type="area"
+                class="unit-sold-apex-chart"
                 :options="options"
                 :series="series"
             />
@@ -24,30 +24,66 @@
     </div>
 </template>
 <style scoped>
-.income-chart .btn-status{
+.chart-area  .btn-status{
     @apply w-[15px] h-[15px] bg-[#0077ac] rounded-full text-black text-[10px] font-bold m-auto;
 }
-.income-chart{
+.chart-area{
     @apply border-2 border-[#0077ac] rounded-[15px] p-[10px];
 }
-.income-chart .header{
+.chart-area .header{
     @apply mt-[5px] mb-[10px];
 }
+
 </style>
 <script setup>
 
+const props = defineProps({
+    data: {
+        type: Array,
+        default: [],
+    },
+    title: {
+        type: Array,
+        default: [],
+    },
+    lastWeek: {
+        type: Number,
+        default: 0.10,
+    },
+    colorLightBlue: {
+        type: Boolean,
+        default: false
+    }
+})
+
 const series= [{
     name: 'comming',
-    data: [31, 80, 68, 109, 92]
-    }];
+    data: props.data
+}];
+
 const options ={
     chart:{
         height : 350,
         type: 'area',
-        background: 'linear-gradient(180deg, rgba(4,55,92,0 ) 0.00%, #04375c 100.00%)',
+        background: props.colorLightBlue ? 'linear-gradient(180deg, rgba(4,55,92,0 ) 0.00%, rgba(63,192,255,0.72 ) 100.00%)' : 'linear-gradient(180deg, rgba(4,55,92,0 ) 0.00%, #04375c 100.00%)',
         toolbar:{
             show: false,
         },
+        events: {
+        mouseMove: function(event, chartContext, config) {
+            var tooltip = chartContext.el.querySelector('.apexcharts-tooltip');
+            var pointsArray = config.globals.pointsArray;
+            var seriesIndex = config.seriesIndex;
+            var dataPointIndex = config.dataPointIndex === -1 ? 0 : config.dataPointIndex;
+
+            if (seriesIndex !== -1) {
+                var position = pointsArray[seriesIndex][dataPointIndex];
+
+                tooltip.style.top = position[1] + 'px';
+                tooltip.style.left = position[0] + 'px';
+            }
+        }
+    }
     },
     dataLabels: {
         enabled : false
@@ -76,22 +112,22 @@ const options ={
         }
     },
     yaxis:{
-        show : false
+        show : false,
     },
     grid: {
         show : false
     },
     fill:{
-        
         type: 'gradient',
+        colors: props.colorLightBlue ? '#3FC0FF' : '#008efa',
         gradient:{
-            shade : 'dark',
+            shade : props.colorLightBlue ? 'light' : 'dark',
             type: 'vertical',
-            shadeIntensity : 1,
-            gradientToColors: ['#0C1C31', '#0077ac'],
+            shadeIntensity : props.colorLightBlue ? 10 : 1,
+            gradientToColors: props.colorLightBlue ? ['#003650', '#3FC0FF'] : ['#0C1C31', '#0077ac'],
             inverseColors : true,
-            opacityTo : 1,
-            opacityFrom : 1,
+            opacityTo : props.colorLightBlue ? 6 : 1,
+            opacityFrom : props.colorLightBlue ? 10 : 1,
         }
     },
     markers: {
@@ -113,7 +149,7 @@ const options ={
                     <span style="color: grey;">Earning</span>
                     <p>$${series[seriesIndex][dataPointIndex]}</p>
                 </div>`
-            );
+            )
         }
     },
 
