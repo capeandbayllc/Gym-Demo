e2e40434c0c34bacd5d7fe8368f883d715440563
Abimael <abimaelc0618@gmail.com>
2023-04-21 21:55:39 -0300

    add the recent-location and recent-statics cards with their respective charts

---
+++ pages/widget-center/components/dashboard-card/components/recent-location-card/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <card-widget title="Statistics 2">
+        <template #summary>
+            <div class="total-sale"><span class="total-symbol">$</span>18,000</div>
+        </template>
+        <template #content>
+            <div class="total-sale"><span class="total-symbol">$</span>18,000</div>
+            <div class="flex flex-row items-center py-2 rounded-xl border border-base-content border">
+                <div class="px-3">
+                    <p>Most Recent Location</p>
+                    <p class="text-base-content">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id commodi reprehenderit aspernatur quibusdam minus.
+                    </p>
+                </div>
+            </div>
+        </template>
+    </card-widget>
+</template>
+<script setup>
+import CardWidget from '../../../card-widget.vue';
+</script>
