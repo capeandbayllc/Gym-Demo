db9e46cd9e2e5eab9bac92fb9b5545681399eddc
Abimael <abimaelc0618@gmail.com>
2023-04-21 23:13:39 -0300

    add statistic-progression card to the widget-center page in the dashboard section

---
+++ pages/widget-center/components/dashboard-card/components/statistic-progression-card/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <card-widget title="Statistics 3">
+        <template #summary>
+            <div class="total-sale">27</div>
+        </template>
+        <template #content>
+            <div class="total-sale">27</div>
+            <div class="flex flex-row justify-center pr-5 items-center py-2 rounded-xl border border-base-content border">
+                <div>
+                    <chart />
+                </div>
+            </div>
+        </template>
+    </card-widget>
+</template>
+
+<script setup>
+import Chart from './chart';
+import CardWidget from '../../../card-widget.vue';
+</script>
