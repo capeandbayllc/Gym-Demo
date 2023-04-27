56c7b6094fff8c74738db0092defb66a4b86dd20
David Duncan <devduncan89@gmail.com>
2023-04-27 17:31:57 -0400

    added screen

--- pages/check-in/profile-card/add-member/information.vue
+++ pages/check-in/profile-card/add-member/information.vue
@@ -1,22 +1,15 @@
 <template>
-    <simple-card class="text-center p-4 w-[450px]">
-        <h2 class="text-lg font-semibold my-2 mb-6">Please enter your information below:</h2>
-        <div class="flex w-fit mx-auto gap-8">
-            <button>
-                <CheckCircleBlueIcon class="mb-4"/>
-                <p>Yes</p>
-            </button>
-            <button>
-                <CrossCircleRedIcon class="mb-4"/>
-                <p>No</p>
-            </button>
-        </div>
-    </simple-card>
+  <simple-card class="text-center p-4 w-[450px]">
+    <h2 class="text-lg font-semibold my-2 mb-6">
+      Please enter your information below:
+    </h2>
+    <input
+      class="gr-base-textfield gr-secondary-textfield"
+      type="text"
+      v-model="inputValue"
+    />
+  </simple-card>
 </template>
 <script setup>
-import {CheckCircleBlueIcon,CrossCircleRedIcon} from '@/components/icons';
-
-const name = ref("Kevin Buchanan");
-const email = ref("kevinbuchanan@gmail.com");
-
+const inputValue = ref("kevinbucanan@gmail.com");
 </script>
