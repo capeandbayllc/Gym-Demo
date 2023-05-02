bd321de905b43408063c64bb15ae24d69a4a232a
Abimael <abimaelc0618@gmail.com>
2023-05-02 14:44:28 -0300

    add a back button to mass comms on the scheduled and builder pages

--- pages/mass-com/scheduled/index.vue
+++ pages/mass-com/scheduled/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pb-4 w-full h-fit pr-5">
+  <div class="pb-4 w-full h-fit pr-12 pl-9">
     <div class="pl-10 pr-4 mb-2 flex flex-1">
       <div class="flex flex-auto">Mass Comms</div>
       <div class="flex gap-2 items-end pb-2 pr-10">
