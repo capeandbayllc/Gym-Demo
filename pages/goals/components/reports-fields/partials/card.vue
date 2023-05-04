87d164a548851dddbb106c9e02cc7a3f97f1af32
Abimael <abimaelc0618@gmail.com>
2023-05-04 20:24:53 -0300

    start goals page, add navbar widget, create report fields card component and add actuals-card

---
+++ pages/goals/components/reports-fields/partials/card.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card-container" :class="{ collapsed: isCollapsed }">
+    <div class="card-content">
+      <div class="card-title-container">
+        <slot name="header" />
+        <div class="card-actions">
+          <div class="w-5">
+            <collapse-btn :size-full="true" v-model="isCollapsed" />
+          </div>
+        </div>
+      </div>
+      <slot v-if="isCollapsed" name="summary" />
+      <slot v-else name="content" />
+    </div>
+  </div>
+</template>
+<style scoped lang="postcss">
+.card-container {
+  @apply py-4 pr-5 w-full;
+  .card-content {
+    @apply border border-secondary rounded-2xl bg-base-300 px-8 py-5 text-left w-full max-w-[1600px];
+  }
+  .card-title-container {
+    @apply flex justify-between items-center;
+    .card-title {
+      @apply font-bold;
+    }
+    .card-actions {
+      @apply flex flex-row space-x-2 cursor-pointer items-center;
+    }
+  }
+}
+.card-container.collapsed {
+  @apply h-fit;
+}
+</style>
+<script setup>
+import CollapseBtn from "~~/components/buttons/collapse-btn.vue";
+
+const isCollapsed = ref(false);
+</script>
---
+++ pages/goals/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="pb-5 pt-4 mx-auto w-full px-10 max-w-[1600px]">
+    <div class="pl-10 pr-4 mb-2 flex justify-content">
+      <div class="flex flex-auto">Goals</div>
+    </div>
+    <div class="page-content gap-5">
+      <report-fields />
+      <!-- <dashboard-card />
+        <reporting-card />
+        <sales-funnel-card />
+        <statistics-card /> -->
+    </div>
+  </div>
+</template>
+<style scoped lang="postcss">
+.page-content {
+  @apply text-center p-6 border-secondary border-2 rounded-[26px] bg-secondary-focus max-w-[85vw];
+}
+</style>
+<script setup>
+import ReportFields from "./components/reports-fields/index.vue";
+//   import ReportingCard from "./components/reporting-card/index.vue";
+//   import SalesFunnelCard from "./components/sales-funnel-card/index.vue";
+//   import StatisticsCard from "./components/statistics/index.vue";
+</script>
