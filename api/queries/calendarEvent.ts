d4f93f94f0b1ea4ab360f01b11c0005210bf7894
Abimael <abimaelc0618@gmail.com>
2023-05-08 19:17:21 -0300

    fix indentation to 2 spaces in the queries/calendarEvents and resolver/calendarEvents files

--- api/queries/calendarEvent.ts
+++ api/queries/calendarEvent.ts
@@ -4,61 +4,61 @@ import { GraphQLQueryInterface } from "~/api/queries/GraphQLQueryInterface";
 
 const GET_CALENDAR_EVENTS_BY_USER: DocumentNode = gql`
   query calendarEvents($param: CalendarEventInput) {
-      calendarEventTypes {
-          data {
-              id
-              name
-              description
-              color
-              type
-          }
-      }
-      calendarEvents(param: $param) {
-          id
-          title
-          owner_id
-          color
-          start
-          end
-          title
-          description
-          full_day_event
-          event_type_id
-          location_id
-          location {
-              id
-              name
-          }
-          type {
-              id
-              name
-              description
-              color
-              type
-          }
-          attendees {
-              id
-              entity_id
-          }
-      }
-      employee {
+    calendarEventTypes {
       data {
-          id
-          user_id
-          first_name
-          last_name
-          email
+        id
+        name
+        description
+        color
+        type
       }
+    }
+    calendarEvents(param: $param) {
+      id
+      title
+      owner_id
+      color
+      start
+      end
+      title
+      description
+      full_day_event
+      event_type_id
+      location_id
+      location {
+        id
+        name
       }
-      members {
-          data {
-              id
-              first_name
-              last_name
-              email
-              profile_photo_path
-          }
+      type {
+        id
+        name
+        description
+        color
+        type
       }
+      attendees {
+        id
+        entity_id
+      }
+    }
+    employee {
+      data {
+        id
+        user_id
+        first_name
+        last_name
+        email
+      }
+    }
+    members {
+      data {
+        id
+        first_name
+        last_name
+        email
+        profile_photo_path
+      }
+    }
   }
 `;
 
@@ -68,8 +68,8 @@ export interface CalendarEventQuery {
 
 const calendarEvent: GraphQLQueryInterface<CalendarEventQuery> = {
   query: {
-      browse_by_user: GET_CALENDAR_EVENTS_BY_USER
-  }
+    browse_by_user: GET_CALENDAR_EVENTS_BY_USER,
+  },
 };
 
 export default calendarEvent;
--- api/queries/resolver/calendarEvents.ts
+++ api/queries/resolver/calendarEvents.ts
@@ -1,22 +1,31 @@
 import { GraphQLResolveInfo } from "graphql/type/definition";
 import { CalendarEventArgs } from "~/api/queries/filters/FilterInterfaces";
 
-export default async function make(_: null, args: CalendarEventArgs, context: object, info: GraphQLResolveInfo) {
+export default async function make(
+  _: null,
+  args: CalendarEventArgs,
+  context: object,
+  info: GraphQLResolveInfo
+) {
+  // @ts-ignore
+  const schema =
+    context.mirageSchema[context.mirageSchema.toCollectionName(info.fieldName)];
+
+  let { param } = args;
+  if (param) {
+    delete args.param;
+  }
+
+  let records =
+    Object.keys(args).length > 0
+      ? schema.where(args).models
+      : schema.all().models;
+
+  // filter
+  if (param?.viewUser) {
     // @ts-ignore
-    const schema = context.mirageSchema[context.mirageSchema.toCollectionName(info.fieldName)];
+    records = records.filter((e) => e.owner_id == param.viewUser);
+  }
 
-    let { param } = args;
-    if(param){
-        delete args.param;
-    }
-
-    let records = (Object.keys(args).length > 0) ? schema.where(args).models : schema.all().models;
-
-    // filter
-    if(param?.viewUser){
-        // @ts-ignore
-        records = records.filter(e => e.owner_id == param.viewUser);
-    }
-
-    return records;
+  return records;
 }
