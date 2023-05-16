fad77698f360b33a3eee163a5a1cbab0def966f7
Abimael <abimaelc0618@gmail.com>
2023-05-16 20:18:38 -0300

    add reporting-table component, add body and head components

---
+++ pages/reporting/all-reports/components/create-report/components/reporting-table.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="card-container">
+    <div class="max-w-full rounded-xl overflow-auto max-h-[70vh] min-h-[102%]">
+      <table class="rounded-2xl">
+        <head-reporting-table />
+        <body-reporting-table :data="data" />
+      </table>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+.card-container {
+  @apply w-full pb-[8px] h-[70vh] overflow-visible border border-secondary rounded-2xl bg-base-300 text-left max-w-full overflow-auto mx-auto;
+
+  .card-title-container {
+    @apply flex justify-between items-center;
+  }
+
+  .card-content {
+    @apply h-full mx-3;
+  }
+}
+
+table {
+  @apply w-[99.5%];
+}
+</style>
+<script setup>
+import HeadReportingTable from "./head-reporting-table.vue";
+import BodyReportingTable from "./body-reporting-table.vue";
+
+const props = defineProps({
+  data: {
+    type: Array,
+    default: [],
+  },
+});
+</script>
