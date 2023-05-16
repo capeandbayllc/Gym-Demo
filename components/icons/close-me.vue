d15536dc47f507b9ca5df55a9b392a8af40e6103
Abimael <abimaelc0618@gmail.com>
2023-05-15 21:43:19 -0300

    refactor icons/close-me icon component

--- components/icons/close-me.vue
+++ components/icons/close-me.vue
@@ -1,46 +1,68 @@
 <template>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="32"
-    height="32"
-    viewBox="0 0 32 32"
-    :class="size == 'big' ? 'w-6 h-6 rounded-full' : 'w-4 h-4 rounded-full'"
-  >
-    <g id="close_me_685" transform="translate(2 2)">
-      <g
-        id="Ellipse_1500"
-        data-name="Ellipse 1500"
-        stroke="#fff"
-        stroke-width="2"
-      >
-        <circle cx="14" cy="14" r="14" stroke="none" />
-        <circle cx="14" cy="14" r="15" fill="none" />
-      </g>
-      <line
-        id="Line_892"
-        data-name="Line 892"
-        y2="16"
-        transform="translate(19.657 8.343) rotate(45)"
-        fill="none"
-        stroke="#fff"
-        stroke-linecap="round"
-        stroke-width="2"
-      />
-      <line
-        id="Line_893"
-        data-name="Line 893"
-        x1="15"
-        transform="translate(8.697 8.697) rotate(45)"
-        fill="none"
-        stroke="#fff"
-        stroke-linecap="round"
-        stroke-width="2"
-      />
-    </g>
-  </svg>
+  <div class="cross-container">
+    <div
+      class="border-[2px] p-[3px] overflow-hidden rounded-full hover:bg-secondary"
+    >
+      <div
+        class="cross"
+        :class="{ 'w-6 h-6 rounded-full': size == 'big' }"
+      ></div>
+    </div>
+  </div>
 </template>
+
 <script setup>
 const props = defineProps({
   size: String,
 });
 </script>
+
+<style scoped lang="postcss">
+.cross {
+  @apply relative w-4 h-4 rotate-0 hover:bg-secondary mr-[1px];
+}
+
+.cross::before,
+.cross::after {
+  content: "";
+  @apply absolute bg-base-content transition-all duration-300;
+}
+
+.cross::before {
+  @apply top-[46%] left-[4%] w-full h-[2px] rounded opacity-100 rotate-[-45deg];
+}
+
+.cross::after {
+  @apply top-0 left-[50%] w-[2px] h-[100%] rounded opacity-100 rotate-[-45deg];
+}
+
+.cross-container:hover .cross::before {
+  @apply opacity-0;
+}
+
+.cross-container:hover .cross::after {
+  animation: rotateAnimation 0.3s ease forwards;
+}
+
+.cross::after {
+  animation: rotateReverseAnimation 0.5s ease both;
+}
+
+@keyframes rotateAnimation {
+  from {
+    transform: rotate(-45deg);
+  }
+  to {
+    transform: rotate(-135deg);
+  }
+}
+
+@keyframes rotateReverseAnimation {
+  from {
+    transform: rotate(-135deg);
+  }
+  to {
+    transform: rotate(-45deg);
+  }
+}
+</style>
