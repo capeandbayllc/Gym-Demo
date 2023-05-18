81018962e0fa08131f02e5a5895c7713ac1e5756
Abimael <abimaelc0618@gmail.com>
2023-05-18 14:03:16 -0300

    implement subfolders for the Report Queue folder

--- pages/reporting/all-reports/index.vue
+++ pages/reporting/all-reports/index.vue
@@ -5,13 +5,13 @@
     </div>
     <div class="page-content gap-5">
       <div class="flex justify-between flex-wrap gap-3">
-        <h3 class="text-2xl font-semibold">{{ actualFolder }}</h3>
+        <h3 class="text-2xl font-semibold">{{ actualFolder.name }}</h3>
         <div class="flex gap-3 items-center pb-2">
           <div class="all-reports-search col-span-4">
             <input
               class="search-input"
               type="text"
-              :placeholder="`Search ${actualFolder}`"
+              :placeholder="`Search ${actualFolder.name}`"
             />
           </div>
           <Button
@@ -27,7 +27,13 @@
       <div class="grid grid-cols-1 md:grid-cols-4 md:gap-3 mt-2">
         <ReportsFoldersCard
           :actual-folder="actualFolder"
-          @changeActualFolder="actualFolder = $event"
+          :actual-sub-folder="actualSubFolder"
+          @changeActualFolder="
+            actualFolder = $event;
+            actualSubFolder = '';
+          "
+          @changeActualSubFolder="actualSubFolder = $event"
+          :folders="folders"
         />
         <ReportsTable :data="data" class="col-span-3 mt-3 md:mt-0" />
       </div>
@@ -80,7 +86,53 @@ import { getRandomInt } from "~/api/utils/number";
 import NewReport from "./components/create-report/new-report.vue";
 import Reporting from "./components/create-report/reporting.vue";
 
-const actualFolder = ref("My Reports");
+const subFolders = ref([
+  "Campaign Reports",
+  "Communication Reports",
+  "Email Reports",
+  "Financial Reports",
+  "Fitness Reports",
+  "Lead Reports",
+  "Member Reports",
+  "People Reports",
+]);
+const folders = ref([
+  {
+    name: "All Reports",
+    subFolders: subFolders.value,
+  },
+  {
+    name: "Report Queue",
+    subFolders: [
+      "Account and Contact Reports",
+      "Deal Reports",
+      "Campaign Reports",
+      "Case and Solution Reports",
+      "Product Reports",
+      "Vendor Reports",
+      "Quote Reports",
+      "Sales Orders Reports",
+    ],
+  },
+  {
+    name: "Favorites",
+    subFolders: subFolders.value,
+  },
+  {
+    name: "Recently Viewed",
+    subFolders: subFolders.value,
+  },
+  {
+    name: "Scheduled Reports",
+    subFolders: subFolders.value,
+  },
+  {
+    name: "Recently Deleted",
+    subFolders: subFolders.value,
+  },
+]);
+const actualFolder = ref(folders.value[0]);
+const actualSubFolder = ref("");
 
 const createReportScreens = ref([NewReport, Reporting]);
 const createReportScreenIndex = ref(0);
@@ -88,10 +140,15 @@ const createReportModal = ref(false);
 
 const data = computed(() => {
   let array = [];
-  for (let i = 0; i < getRandomInt(actualFolder.value.length * 3, 0); i++) {
+  let actualName =
+    actualSubFolder.value == ""
+      ? actualFolder.value.name
+      : actualSubFolder.value;
+  console.log(actualName);
+  for (let i = 0; i < getRandomInt(actualName.length * 3, 0); i++) {
     array.push({
       id: 1,
-      report_name: actualFolder.value,
+      report_name: actualName,
       description: "",
       report_type: "",
       date_created: "",
