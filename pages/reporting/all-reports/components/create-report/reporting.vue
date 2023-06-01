b7925fd2e590c80d53f0b66b20faf3c65055470c
Abimael <abimaelc0618@gmail.com>
2023-06-01 18:46:26 -0300

    add reporting-actions component and implement it in the reporting for code cleanliness and reusability

--- pages/reporting/all-reports/components/create-report/reporting.vue
+++ pages/reporting/all-reports/components/create-report/reporting.vue
@@ -1,84 +1,11 @@
 <template>
   <div class="modal-content">
     <div class="grid grid-cols-1 sm:grid-cols-12 gap-3 mt-2">
-      <div
-        class="relative flex flex-wrap items-center gap-5 sm:col-span-4 md:col-span-4 xl:col-span-3"
-      >
-        <div class="flex items-center gap-3">
-          <arrow-left
-            class="pt-1 min-w-12 max-w-12 h-12 cursor-pointer"
-            @click="emit('back')"
-          ></arrow-left>
-          <h5 class="text-2xl whitespace-nowrap mr-2">
-            {{ report.report_name }}
-          </h5>
-          <div class="relative">
-            <button @click.stop="toggleTitleDropdown">
-              <vertical-ellipsis
-                class="h-6 fill-base-content transform rotate-90 mt-[9px]"
-              />
-            </button>
-            <div class="dropdown" v-show="showTitleDropdown">
-              <div class="dropdown-container">
-                <div class="dropdown-item" @click="openRenameModal">Rename</div>
-                <div class="dropdown-item" @click.stop="toggleTitleDropdown">
-                  Delete
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div
-        class="flex flex-wrap justify-between gap-4 items-center sm:col-span-8 md:col-span-8 xl:col-span-9"
-      >
-        <div class="flex items-center gap-8">
-          <span class="text-lg font-semibold" v-if="actualSection == 'Columns'">
-            Leads
-          </span>
-          <div class="flex gap-1 flex-nowrap items-start">
-            <font-awesome-icon
-              :icon="['fas', 'circle-info']"
-              size="md"
-              class="mr-1 mt-[4px] focus:outline-none"
-              tabindex="0"
-              @click.prevent.stop
-            />
-            <p>
-              Do you want to run another report?
-              <button
-                @click="openCreateJoinedReportModal"
-                class="text-secondary font-semibold"
-              >
-                Joined Report
-              </button>
-            </p>
-          </div>
-        </div>
-
-        <div class="flex gap-4 ml-auto">
-          <Button
-            size="sm"
-            outline
-            class="normal-case rounded-lg hover:text-secondary"
-            @click="emit('close')"
-          >
-            Cancel
-          </Button>
-          <Button
-            size="sm"
-            outline
-            class="normal-case rounded-lg hover:text-secondary"
-          >
-            Run
-          </Button>
-          <edit-report>
-            <button class="bg-secondary rounded-l-xl px-5">Save</button>
-          </edit-report>
-        </div>
-      </div>
-
+      <reporting-actions
+        :report="report"
+        :actualSection="actualSection"
+        @changeReport="emit('changeReport', $event)"
+      />
       <sidebar
         :actualSection="actualSection"
         :columns="columnColumns"
@@ -92,24 +19,6 @@
       />
     </div>
   </div>
-  <daisy-modal :overlay="true" :show-close-button="false" ref="renameModal">
-    <rename-modal
-      @close="closeRenameModal"
-      :report-name="report.report_name"
-      @changeName="changeName"
-      :open="renameModal?.isOpen"
-    />
-  </daisy-modal>
-  <daisy-modal
-    :overlay="true"
-    :show-close-button="false"
-    ref="createJoinedReportModal"
-  >
-    <create-joined-report-modal
-      @close="closeCreateJoinedReportModal"
-      :report-name="report.report_name"
-    />
-  </daisy-modal>
 </template>
 
 <style scoped lang="postcss">
@@ -118,30 +27,14 @@
   ::-webkit-scrollbar {
     @apply hidden;
   }
-  .dropdown {
-    @apply absolute top-[28px] right-[0px] h-auto z-[60];
-    .dropdown-container {
-      @apply bg-base-300 border-secondary border text-base font-normal rounded-2xl p-3 px-5 w-[120px];
-      .dropdown-item {
-        @apply mb-2 cursor-pointer;
-      }
-    }
-  }
 }
 </style>
 
 <script setup>
 import Sidebar from "./components/sidebar/index.vue";
 import ReportingTable from "./components/reporting-table.vue";
-import RenameModal from "../rename-modal.vue";
 import { getRandomInt } from "~/api/utils/number";
-import EditReport from "../edit-report/index.vue";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
-import { VerticalEllipsis, ArrowLeft } from "~/components/icons";
-import CreateJoinedReportModal from "./components/create-joined-report-modal.vue";
-library.add(faInfoCircle);
+import ReportingActions from "./components/reporting/reporting-actions.vue";
 
 const props = defineProps({
   report: {
@@ -150,34 +43,9 @@ const props = defineProps({
   },
 });
 
-const showTitleDropdown = ref(false);
-
-const emit = defineEmits(["close", "back", "next", "changeReportName"]);
+const emit = defineEmits(["close", "back", "next", "changeReport"]);
 
 const actualSection = ref("Columns");
-
-const toggleTitleDropdown = () => {
-  showTitleDropdown.value = !showTitleDropdown.value;
-};
-
-const renameModal = ref(null);
-
-const openRenameModal = () => {
-  toggleTitleDropdown();
-  renameModal.value.open();
-};
-const closeRenameModal = () => {
-  renameModal.value.close();
-};
-
-const createJoinedReportModal = ref(null);
-
-const openCreateJoinedReportModal = () => {
-  createJoinedReportModal.value.open();
-};
-const closeCreateJoinedReportModal = () => {
-  createJoinedReportModal.value.close();
-};
 
 const actualData = computed(() => {
   if (["Columns", "SelectColumns"].includes(actualSection.value)) {
@@ -186,6 +54,7 @@ const actualData = computed(() => {
     return dataFilters.value;
   }
 });
+
 const dataColumns = computed(() => {
   let array = [];
   for (let i = 0; i < getRandomInt(20 * 3, 0); i++) {
@@ -201,6 +70,7 @@ const dataColumns = computed(() => {
   }
   return array;
 });
+
 const dataFilters = computed(() => {
   let array = [];
   for (let i = 0; i < getRandomInt(20 * 3, 0); i++) {
@@ -223,10 +93,6 @@ const actualColumns = computed(() => {
     return columnsFilters.value?.filter((column) => column.active);
   }
 });
-
-const changeName = (event) => {
-  emit("changeReportName", event);
-};
 
 const columnColumns = ref([
   {
